b3744aa71ffe9e5f0d9d7baa6a08185d7f505328
MCC <cocahack@naver.com>
2019-06-11 22:14:18 +0900

    feat: todo 추가하는 js 코드 삽입
    - add card 버튼 클릭 시 버튼을 input 태그로 변경
    - input 태그에서 엔터를 누르면 todo가 등록

--- public/index.js
+++ public/index.js
@@ -1,20 +1,63 @@
+function getTodoListContainer (title) {
+  return `<div class="todo-column todolist-container grey lighten-2">
+<div class="todolist-header">
+  ${title}
+</div>
+<ul class="collection">
+</ul>
+<div class="todolist-footer">
+  <button class="add-card">Add card</button>
+  <div class="add-card-form hide-element">
+    <input class="input-field new-card-name" placeholder="Input new card name">
+  </div>
+</div>
+</div>
+`;
+}
+
+function getTodoItem (value) {
+  return `<li class="collection-item todo-item"><div>${value}</div></li>`
+}
+
+function appendChildInTargetNode(target, htmlText) {
+  var parser = new DOMParser();
+  var newDOM = parser.parseFromString(htmlText, 'text/html');
+  target.appendChild(newDOM.body.firstChild);
+}
+
 function onClickListenerOfAddCard (event) {
   event.preventDefault();
   this.classList.add('hide-element');
-  var form = this.parentNode.getElementsByClassName('add-card-form');
-  console.log(form);
-  form[0].classList.remove('hide-element');
+  var form = this.parentNode.querySelector('.add-card-form');
+  form.classList.remove('hide-element');
+  form.querySelector('.new-card-name').focus();
 }
 
-// function pressEnterListener (event) {
-//   event.preventDefault();
-//   var cardName = this.value;
-//   if(cardName === undefined)  
-// }
+function newCardNameInputTagListener (event) {
+  var key = event.which || event.keyCode;
+
+  if(key === 13){
+    if(this.value === '') return;
+    var target = this.parentNode.parentNode.parentNode.querySelector('.collection');
+    appendChildInTargetNode(target, getTodoItem(this.value));
+
+    var addCardBtn = this.parentNode.parentNode.querySelector('.add-card');
+    addCardBtn.classList.remove('hide-element');
+    this.parentNode.classList.add('hide-element');
+    this.value = "";
+  }
+
+  // var cardName = this.value;
+  // if(cardName === undefined)  
+}
 
 var selectAddCardBtn = document.getElementsByClassName('add-card');
-console.log(selectAddCardBtn);
+var newCardNameInput = document.getElementsByClassName('new-card-name');
 
 for(var i=0; i<selectAddCardBtn.length; ++i){
   selectAddCardBtn[i].addEventListener('click', onClickListenerOfAddCard);
+}
+
+for(var i=0; i<newCardNameInput.length; ++i){
+  newCardNameInput[i].addEventListener('keypress', newCardNameInputTagListener);
 }
