0a9e93f66c0e06ecee9a2f27a8390698a38c0195
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-13 16:58:02 +0900

    Feat : element를 만드는 createElemnet 메서드 추가
    element를 만드는 유틸성 메서드

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 const Index = class {
-    constructor() {
+  constructor() {
 
+  }
+
+  createElement(elName, classNameArr, attObj, innerHtml) {
+    const element = document.createElement(elName);
+    if (classNameArr !== null) {
+      classNameArr.forEach(name => {
+        element.classList.add(name);
+      })
     }
+
+    Object.keys(attObj).forEach((key) => {
+      element.setAttribute(key, attObj[key]);
+    })
+
+    if (innerHtml !== null) {
+      element.innerHTML = innerHtml;
+    }
+    return element;
+  }
+
+  
+
 }
