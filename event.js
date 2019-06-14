fb817524b3975791327f4b2e1fcda2e2ccbd4b29
LeeJuyeon <ruby413@hanmail.net>
2019-06-14 20:39:45 +0900

    refactor : refactor ing

--- event.js
+++ event.js
@@ -4,11 +4,6 @@ const display = (displayID, displayNoneID) => {
     document.getElementById(displayNoneID).style.display="none";
 }
 
-const displayNone = (displayNoneID) => {
-    document.getElementById(displayNoneID).style.display="none";
-}
-
-
 const callAppendChild = (targetId, valueId) => { 
     const data = document.getElementById(valueId).value;
     const text = document.createTextNode(data); 
@@ -23,9 +18,40 @@ const callAppendChild = (targetId, valueId) => {
         span.classList.add("icon-hamburger-menu-close")
         target.appendChild(li); 
         document.getElementById(valueId).value = "";   
+        
+        // li.setAttribute('draggable', true); 
+        // li.setAttribute('ondragstart', "drag(event)"); 
 
         li.addEventListener("mouseover", ()=>{span.style.display = "block";}, false)
         li.addEventListener("mouseout", ()=>{span.style.display = "none";}, false)
         span.addEventListener("click", ()=>{target.removeChild(li);}, false)
     }
 }
+
+// function allowDrop(ev) {
+//     var cols = document.querySelectorAll('#drag-list_.drag-row');
+//    var colsLength = cols.length;
+
+//    for (var i = 0; i < colsLength; i++) 
+//    {
+//       li.addEventListener('dragstart', dragStart, false);
+//       li.addEventListener('drag', dragIng, false);
+//       li.addEventListener('dragenter', dragEnter, false);
+//       li.addEventListener('dragover', dragOver, false);
+//       li.addEventListener('dragleave', dragLeave, false);
+//       li.addEventListener('drop', dragDrop, false);
+//       li.addEventListener('dragend', dragEnd, false)
+// //    }
+// function allowDrop(ev) {
+//     ev.preventDefault();
+// }
+ 
+// function drag(ev) {
+//     ev.dataTransfer.setData("text", ev.target.id);
+// }
+ 
+// function drop(ev) {
+//     ev.preventDefault();
+//     var data = ev.dataTransfer.getData("text");
+//     ev.target.appendChild(document.getElementById(data));
+// }
