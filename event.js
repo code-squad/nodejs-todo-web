7bd6652ea3337cc06036e9f9a55e4becadcc7d03
LeeJuyeon <ruby413@hanmail.net>
2019-06-14 16:00:06 +0900

    create : index.html, css, js

--- event.js
+++ event.js
@@ -1,54 +1,25 @@
-const display = (displayID, displayNoneID) => {
-    document.getElementById(displayID).style.display="inherit";
-    document.getElementById(displayNoneID).style.display="none";
+const display = (displayClass, displayNoneClass) => {
+    document.getElementById(displayClass).style.display="inherit";
+    document.getElementById(displayNoneClass).style.display="none";
 }
 
-const callAppendChild = (targetId, valueId) => { 
-    const data = document.getElementById(valueId).value;
-    const text = document.createTextNode(data); 
-    const targetUl = document.getElementById(targetId); 
-    const li = document.createElement('li'); 
-    const span = document.createElement('span'); 
 
+const callAppendChild = (targetId) => { 
+    let data = document.getElementById("addContents").value;
+    let text = document.createTextNode(data); 
+    let target = document.getElementById(targetId); 
+    let li = document.createElement('li'); 
+    let span = document.createElement('span'); 
     if(data !== ""){
         li.classList.add("contents")
         li.appendChild(text); 
         li.appendChild(span); 
         span.classList.add("deleteData")
         span.classList.add("icon-hamburger-menu-close")
-        targetUl.appendChild(li); 
-        document.getElementById(valueId).value = "";   
+        target.appendChild(li); 
+        document.getElementById("addContents").value = "";   
+
         li.addEventListener("mouseover", ()=>{span.style.display = "block";}, false)
         li.addEventListener("mouseout", ()=>{span.style.display = "none";}, false)
-        span.addEventListener("click", (ev)=>{ev.target.parentNode.parentNode.removeChild(ev.target.parentNode);}, false)
-
-        drag(targetUl,li)
     }
-}
-
-const drag = (listWrap, list) => {
-    let dragging = null;
-    list.setAttribute('draggable', true); 
-    document.addEventListener('dragstart', (event) => { 
-        dragging = event.target;
-        event.dataTransfer.setData('text/html', dragging);
-    });
-
-    document.addEventListener('dragover', (event) => {
-        event.preventDefault();
-    });
-
-    listWrap.addEventListener('dragenter', (event) => {
-        event.target.style['border-bottom'] = 'solid 2px blue';
-    });
-
-    listWrap.addEventListener('dragleave', (event) => {
-        event.target.style['border-bottom'] = '';
-    });
-
-    listWrap.addEventListener('drop', (event) => {
-        event.preventDefault();
-        event.target.style['border-bottom'] = '';
-        event.target.parentNode.insertBefore(dragging, event.target.nextSibling);
-    });
 }
