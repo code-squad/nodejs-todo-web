cf5f99c3de2a96a54e5d32235bba09bf521363ec
LeeJuyeon <ruby413@hanmail.net>
2019-06-14 19:54:20 +0900

    create : make doing and done

--- event.js
+++ event.js
@@ -9,8 +9,8 @@ const displayNone = (displayNoneID) => {
 }
 
 
-const callAppendChild = (targetId) => { 
-    const data = document.getElementById("addContents").value;
+const callAppendChild = (targetId, valueId) => { 
+    const data = document.getElementById(valueId).value;
     const text = document.createTextNode(data); 
     const target = document.getElementById(targetId); 
     const li = document.createElement('li'); 
@@ -22,42 +22,10 @@ const callAppendChild = (targetId) => {
         span.classList.add("deleteData")
         span.classList.add("icon-hamburger-menu-close")
         target.appendChild(li); 
-        document.getElementById("addContents").value = "";   
+        document.getElementById(valueId).value = "";   
 
         li.addEventListener("mouseover", ()=>{span.style.display = "block";}, false)
         li.addEventListener("mouseout", ()=>{span.style.display = "none";}, false)
         span.addEventListener("click", ()=>{target.removeChild(li);}, false)
     }
 }
-
-// const createContainer = () => {
-
-// }
-
-// const createContainer = () => {
-//     const targetWrap = document.getElementById("containerWrap"); 
-//     const htmlTemplate = `<div class = "container">
-//                             <ul id = "dataTarget">
-//                                 <li class = "containSubject">${data}</li>
-//                             </ul>
-//                             <div class = "addDiv">
-//                                 <div id = "openAdd">
-//                                     <span class = "add icon-plus" onclick='display("addData", "openAdd")' >Add another card</span>
-//                                 </div>
-//                                 <div id = "addData">
-//                                     <textarea class="addContents" id ="addContents" placeholder="Enter a title for this card..."></textarea>
-//                                     <span class="inputAdd" id="inputAdd" onclick='display("openAdd", "addData"); callAppendChild("dataTarget")'>Add Card</span>
-//                                     <span class="backButton icon-hamburger-menu-close" onclick='display("openAdd", "addData")'></span>
-//                                 </div>
-//                             </div>
-//                         </div>`
-    
-//     targetWrap.innerHTML += htmlTemplate
-// }
-
-// const deleteData = () =>{
-//     const targetParent = document.getElementById("dataTarget"); 
-//     const deleteButton = document.getElementById("deleteData"); 
-// }
-// var ul = document.getElementById("yourElementId");
-// ul.removeChild(ul.firstChild);
