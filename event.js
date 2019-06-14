479930026febd6710292d686f6fc9671f2933413
LeeJuyeon <ruby413@hanmail.net>
2019-06-14 18:47:22 +0900

    create : make addlist button

--- event.js
+++ event.js
@@ -1,15 +1,20 @@
-const display = (displayClass, displayNoneClass) => {
-    document.getElementById(displayClass).style.display="inherit";
-    document.getElementById(displayNoneClass).style.display="none";
+const display = (displayID, displayNoneID) => {
+    console.log(document.getElementById(displayID));
+    document.getElementById(displayID).style.display="inherit";
+    document.getElementById(displayNoneID).style.display="none";
+}
+
+const displayNone = (displayNoneID) => {
+    document.getElementById(displayNoneID).style.display="none";
 }
 
 
 const callAppendChild = (targetId) => { 
-    let data = document.getElementById("addContents").value;
-    let text = document.createTextNode(data); 
-    let target = document.getElementById(targetId); 
-    let li = document.createElement('li'); 
-    let span = document.createElement('span'); 
+    const data = document.getElementById("addContents").value;
+    const text = document.createTextNode(data); 
+    const target = document.getElementById(targetId); 
+    const li = document.createElement('li'); 
+    const span = document.createElement('span'); 
     if(data !== ""){
         li.classList.add("contents")
         li.appendChild(text); 
@@ -21,5 +26,38 @@ const callAppendChild = (targetId) => {
 
         li.addEventListener("mouseover", ()=>{span.style.display = "block";}, false)
         li.addEventListener("mouseout", ()=>{span.style.display = "none";}, false)
+        span.addEventListener("click", ()=>{target.removeChild(li);}, false)
     }
 }
+
+// const createContainer = () => {
+
+// }
+
+// const createContainer = () => {
+//     const targetWrap = document.getElementById("containerWrap"); 
+//     const htmlTemplate = `<div class = "container">
+//                             <ul id = "dataTarget">
+//                                 <li class = "containSubject">${data}</li>
+//                             </ul>
+//                             <div class = "addDiv">
+//                                 <div id = "openAdd">
+//                                     <span class = "add icon-plus" onclick='display("addData", "openAdd")' >Add another card</span>
+//                                 </div>
+//                                 <div id = "addData">
+//                                     <textarea class="addContents" id ="addContents" placeholder="Enter a title for this card..."></textarea>
+//                                     <span class="inputAdd" id="inputAdd" onclick='display("openAdd", "addData"); callAppendChild("dataTarget")'>Add Card</span>
+//                                     <span class="backButton icon-hamburger-menu-close" onclick='display("openAdd", "addData")'></span>
+//                                 </div>
+//                             </div>
+//                         </div>`
+    
+//     targetWrap.innerHTML += htmlTemplate
+// }
+
+// const deleteData = () =>{
+//     const targetParent = document.getElementById("dataTarget"); 
+//     const deleteButton = document.getElementById("deleteData"); 
+// }
+// var ul = document.getElementById("yourElementId");
+// ul.removeChild(ul.firstChild);
