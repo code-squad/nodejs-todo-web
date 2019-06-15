91d8a9336fca3d48570ba92871335082cba56d0d
mukeunzi <eunji057@gmail.com>
2019-06-15 16:16:53 +0900

    refactor : prototype으로 리팩토링

--- js/app.js
+++ js/app.js
@@ -1,34 +1,38 @@
-let data;
+function TodoFront() {
+	this.dragData = undefined;
+}
 
-window.addEventListener('load', function() {
-	const addTodo = this.document.querySelector('#addTodo');
-	const addButton = this.document.querySelector('#addButton');
-	const todos = this.document.querySelectorAll('.todos');
+TodoFront.prototype.load = function() {
+	window.addEventListener('load', () => {
+		const addTodo = document.querySelector('#addTodo');
+		const addButton = document.querySelector('#addButton');
+		const todos = document.querySelectorAll('.todos');
 
-	addTodo.addEventListener('keyup', function(event) {
-		if (event.keyCode === 13) {
-			addTodoList();
-		}
-	});
+		addTodo.addEventListener('keyup', event => {
+			if (event.keyCode === 13) {
+				this.addTodoList();
+			}
+		});
 
-	addButton.addEventListener('click', function(event) {
-		addTodoList();
-	});
+		addButton.addEventListener('click', event => {
+			this.addTodoList();
+		});
 
-	todos.forEach(element => {
-		element.addEventListener('dragover', function(event) {
-			allowDrop(event);
+		todos.forEach(element => {
+			element.addEventListener('dragover', event => {
+				this.allowDrop(event);
+			});
+		});
+
+		todos.forEach(element => {
+			element.addEventListener('drop', event => {
+				this.drop(event);
+			});
 		});
 	});
+};
 
-	todos.forEach(element => {
-		element.addEventListener('drop', function(event) {
-			drop(event);
-		});
-	});
-});
-
-const addTodoList = () => {
+TodoFront.prototype.addTodoList = function() {
 	const addTodo = document.querySelector('#addTodo').value;
 
 	if (!addTodo) {
@@ -42,50 +46,53 @@ const addTodoList = () => {
 	todoArticle.appendChild(todoContent);
 	todoArticle.className = 'todo todo-list';
 	todoArticle.setAttribute('draggable', 'true');
-	todoArticle.addEventListener('dragstart', function(event) {
-		drag(event);
+	todoArticle.addEventListener('dragstart', event => {
+		this.drag(event);
 	});
 	todo.appendChild(todoArticle);
 	document.querySelector('#addTodo').value = '';
 };
 
-const drag = event => {
-	data = event.target;
+TodoFront.prototype.drag = function(event) {
+	this.dragData = event.target;
 };
 
-const allowDrop = event => {
+TodoFront.prototype.allowDrop = function(event) {
 	event.preventDefault();
 };
 
-const drop = event => {
+TodoFront.prototype.drop = function(event) {
 	const dropAreaClassName = event.target.className.split(' ')[0];
 	const dropAreaList = document.querySelector(`#${dropAreaClassName}`).children;
 
 	if (!dropAreaList.length) {
-		document.querySelector(`#${dropAreaClassName}`).appendChild(data);
+		document.querySelector(`#${dropAreaClassName}`).appendChild(this.dragData);
 	} else {
 		const cursorYLocation = event.clientY;
-		const appendTargetIndex = getAppendTargetIndex(dropAreaList, cursorYLocation);
+		const appendTargetIndex = this.getAppendTargetIndex(dropAreaList, cursorYLocation);
 
 		if (appendTargetIndex === -1) {
-			document.querySelector(`#${dropAreaClassName}`).appendChild(data);
+			document.querySelector(`#${dropAreaClassName}`).appendChild(this.dragData);
 		} else {
-			document.querySelector(`#${dropAreaClassName}`).insertBefore(data, dropAreaList[appendTargetIndex]);
+			document.querySelector(`#${dropAreaClassName}`).insertBefore(this.dragData, dropAreaList[appendTargetIndex]);
 		}
 	}
 };
 
-const getElementMiddleY = element => {
+TodoFront.prototype.getElementMiddleY = function(element) {
 	const elementLocation = element.getBoundingClientRect();
 	return (elementLocation.top + elementLocation.bottom) / 2;
 };
 
-const getAppendTargetIndex = (dropAreaList, cursorYLocation) => {
+TodoFront.prototype.getAppendTargetIndex = function(dropAreaList, cursorYLocation) {
 	const appendTargetIndex = Array.from(dropAreaList).findIndex(element => {
-		const elementMiddleY = getElementMiddleY(element);
+		const elementMiddleY = this.getElementMiddleY(element);
 
 		return elementMiddleY >= cursorYLocation;
 	});
 
 	return appendTargetIndex;
 };
+
+const todoFront = new TodoFront();
+todoFront.load();
