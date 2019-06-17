bc6e9855fa21a9a0db0dce0641f69806f707fb90
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-17 22:45:01 +0900

    Create : server 모듈 생성
    http 모듈을 받아 server를 생성하는 메서드 구현

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hello, node');
+});
+
+module.exports = server;
