f9cb4e49bf4b453c3812564183566615a2d35d69
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-18 19:27:16 +0900

    Feat : 정적 파일을 읽어 응답하는 serveStaticFile 메서드 구현
    해당 경로에서 파일을 찾아 응답

--- src/serve-static.js
+++ src/serve-static.js
@@ -18,6 +18,35 @@ const ServeStatic = class {
   getExt(req) {
     return path.parse(req.url).ext;
   }
+
+  readFile(path) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, (err, data) => {
+        if (err) reject(err);
+        resolve(data);
+      })
+    })
+  }
+
+  async serveStaticFile(req, res) {
+    const ext = this.getExt(req);
+    if (Object.keys(this.mimeType).includes(ext)) {
+      const filePath = `${this.publicPath}${req.url}`;
+    
+      try {
+        const data = await this.readFile(filePath);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', this.mimeType[ext]);
+        res.write(data);
+        res.end();
+      } catch (error) {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.write('Not Found File');
+        res.end();
+      }
+    }
+  }
 }
 
 module.exports = ServeStatic;
