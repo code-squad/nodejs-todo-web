280ac8461b2d646a3e4e8e214d6064927aa53642
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-18 21:06:19 +0900

    Test : 미들웨어를 실행하는 run() 메서드 테스트 작성
    미들웨어를 순서대로 모두 실행

--- test/middleware.spec.js
+++ test/middleware.spec.js
@@ -12,7 +12,6 @@ describe('Middleware Module', () => {
     should(middleware.middlewareArr.length).be.equal(0);
   })
 
-
   describe('add()', () => {
     it('미들웨어 배열에 함수를 추가', () => {
       const functionArr = [
@@ -24,13 +23,40 @@ describe('Middleware Module', () => {
       })
 
       should(middleware.middlewareArr.length).be.equal(3);
+    })
+  })
+
+  describe('run()', () => {
+    it('미들웨어 배열의 함수 실행', () => {
+      // 가짜 함수인 익명 함수 스텁을 만든다
+      const stub1 = sinon.stub();
+      const stub2 = sinon.stub();
+      const stub3 = sinon.stub();
+
+      // 만든 스텁 함수의 동작을 callsFake로 정의한다
+      stub1.callsFake((req, res, next) => next());
+      stub2.callsFake((req, res, next) => next());
+      stub3.callsFake((req, res, next) => next());
+
+      const funcs = [stub1, stub2, stub3];
+      
+      // 미들웨어 배열에 스텁 함수를 등록
+      funcs.forEach((func) => {
+        middleware.middlewareArr.push(func);
+      });
+
+      // run 메서드 실행
+      middleware.run();
+
+      funcs.forEach((func) => {
+        // 메서드가 모두 호춣되었는 지 test
+        should(func.called).equal(true);
+      })
 
     })
-
-
-
-
   })
+
+
 
 
 })
