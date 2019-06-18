e195b6a3394d49a7cbe35e1fb04af8921aabbd37
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-18 14:29:24 +0900

    Feat : tag + msg 메시지 반환하도록 return

--- utils/debug.js
+++ utils/debug.js
@@ -1,7 +1,12 @@
 const debug = (tag) => {
-    if(!tag) {
-        throw Error('tag가 필요합니다');
-    }
+  if (!tag) {
+    throw Error('tag가 필요합니다');
+  }
+
+  return (msg) => {
+    const log = `[${tag}] ${msg}`
+    return log;
+  }
 }
 
 module.exports = debug;
