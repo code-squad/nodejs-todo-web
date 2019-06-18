1d142782958412a23e5bcb4640025c34762f8d97
gminiy <gmini.y@gmail.com>
2019-06-18 14:52:00 +0900

    todo webpage 구현 (#3)
    * [feat]Project init
    
    * [feat] header 추가, header, body style 구현
    
    * [feat] list-box 구현
    
    - list-box-wrapper : display flex, 정렬은 spacearound.
    - list-box : height를 header height와 margin을 브라우져 크기에서  뺀 나머지
    
    * [feat] header margin 조정
    
    * [feat] list-box-header 구현
    
    * [feat] list-box margin 설정, list-box max-hegiht로 변경
    
    - margin을 줘서 browser 크기 변경시에도 최소한의 간격을 유지하게 함
    - height 를 max 로 줘서 리스트 추가시 크기가 변경되게 함
    
    * [feat] list-box border-radius, list-box 안에 list-area 만듬
    
    * [feat] open-add-form 생성
    
    - 해당 form clik하면 add form 이 열리도록 구성할 예정
    - 클릭시 브라우져 가장 상단으로 이동할 수 있도록 a 태그 사용
    
    * [feat] add-form-link click 시 add-form 나타나는 기능 구현
    
    - add-form-link click하면 addForm Template append, add-form-link invisiabl.
    - x 클릭시 반대로 동작.
    
    * [feat] addList function 구현
    
    * [feat] item css 적용
    
    * [feat] add-button 클릭 이벤트 구현
    
    - add button click 하면 textarea 값을 받아서 addItem function 실행
    
    * [feat] list-area overflow scroll
    
    * [feat] drag & drop 구현
    
    - 각 item 에 drag&drop event 설정
    - addDragEvent() : item 에 drag event 부여, drag enter 시 및줄 표시, 떠나면 지우기, drop시 nextSibling before insert.
    
    * [feat] add 시 text area로 focus

---
+++ index.js
@@ -0,0 +1,85 @@
+const addForm = `
+    <div class="writing-item-box">
+        <textarea placeholder="Enter a title for your new item"></textarea>
+    </div>
+    <div class="add-button-form">
+        <button type="button" class="add-button" onclick="addItemByAddButton(event)">ADD</button>
+        <a class="icon-hamburger-menu-close close" href="#" style="font-size: 30px; margin-left: 8px;" onclick="closeAddForm(event)"></a>
+    </div>`;
+const openAddFormLinks = document.getElementsByClassName('open-add-form-link');
+let draggingTarget = null;
+
+for (openAddFormLink of openAddFormLinks) {
+    openAddFormLink.addEventListener('click', (event) => {
+        const listBoxNode = event.target.parentNode;
+        const addFormElement = document.createElement('div');
+        addFormElement.setAttribute('class', 'add-form');
+        addFormElement.innerHTML = addForm;
+        listBoxNode.appendChild(addFormElement);
+        event.target.style.display = 'none';
+        const textArea = listBoxNode.getElementsByTagName('textarea')[0];
+        textArea.focus();
+    });
+}
+
+function generateRandomId() {
+    return Date.now();
+}
+
+function closeAddForm(event) {
+    const addButtonFormNode = event.target.parentNode;
+    const addFormNode = addButtonFormNode.parentNode;
+    const listBoxNode = addFormNode.parentNode;
+    const openAddFormLink = listBoxNode.getElementsByClassName('open-add-form-link')[0];
+    listBoxNode.removeChild(addFormNode);
+    openAddFormLink.style.display = 'inherit';
+}
+
+function addItem(parentNode, name) {
+    const item = document.createElement('div');
+    item.setAttribute('class', 'item');
+    item.setAttribute('id', generateRandomId());
+    item.setAttribute('draggable', true);
+    item.innerHTML = `${name}`;
+    addDragEvent(item);
+    parentNode.appendChild(item);
+}
+
+function addItemByAddButton(event) {
+    const addButtonFormNode = event.target.parentNode;
+    const addFormNode = addButtonFormNode.parentNode;
+    const listBoxNode = addFormNode.parentNode;
+    const listArea = listBoxNode.getElementsByClassName('list-area')[0];
+    const textArea = addFormNode.getElementsByTagName('textarea')[0];
+    const name = textArea.value;
+    if (!(name === '')) {
+        addItem(listArea, name);
+        textArea.value = null;
+    }
+    textArea.focus();
+}
+
+function addDragEvent(item) {
+    item.addEventListener('dragstart', function(event) {
+        draggingTarget = event.target;
+        event.dataTransfer.setData('text/html', draggingTarget);
+    });
+
+    item.addEventListener('dragover', function(event) {
+        event.preventDefault();
+    });
+
+    item.addEventListener('dragenter', function(event) {
+        event.target.style['border-bottom'] = 'solid 3px red';
+    });
+
+    item.addEventListener('dragleave', function(event) {
+        event.target.style['border-bottom'] = '';
+    });
+
+    item.addEventListener('drop', (event) => {
+        event.preventDefault();
+        event.target.style['border-bottom'] = '';
+        event.target.parentNode.insertBefore(draggingTarget, event.target.nextElementSibling);
+    });
+}
