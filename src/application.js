43c98e54418cee4817afc33335aae463fe8f2ab2
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-18 19:29:34 +0900

    Feat : 서버에 serveStatic 모듈 적용
    serveStatic 모듈 적용해 정적 파일 응답하기

--- src/application.js
+++ src/application.js
@@ -1,15 +1,19 @@
 const http = require('http');
 const debug = require('../utils/debug')('Application');
 const path = require('path');
+const ServeStatic = require('../src/serve-static');
 const fs = require('fs');
 
 const Application = class {
   constructor() {
+    this.serveStatic = new ServeStatic();
     this.server =  http.createServer((req, res) => {
-      
       const filePath = path.join(__dirname, '../public/index.html');
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');
+      const str = this.serveStatic.getExt(req);
+      console.log(str);
+      this.serveStatic.serveStaticFile(req, res);
       
       fs.readFile(filePath, (err, data) => {
         if (err) throw err;
