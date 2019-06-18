978024407348cf23d6496f3234689c17f67fcd4b
Min-92 <0201.mj.lee@gmail.com>
2019-06-19 00:15:39 +0900

    feat : set title
    - add setTitle method

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ class Title {
     constructor(name){
         this.name = name;
     }
+
+    setTitle(){
+        const body = document.querySelector('body');
+        const header = document.createElement('header');
+        header.innerHTML = `<h1>${this.name}'s Todo</h1>`;
+        body.insertBefore(header,body.firstChild);
+
+    }
 }
 
-const title = new title;
-
+const title = new Title('Wangmin');
+title.setTitle();
