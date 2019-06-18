9cd873f9f8328bd194ba8684537a9410c116af2a
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-18 14:52:42 +0900

    Feat : listen 메서드에 debug 모듈 적용
    로그 남기기 위한 debug 모듈 적용

--- src/application.js
+++ src/application.js
@@ -1,10 +1,13 @@
+const debug = require('../utils/debug')('App');
+
 const Application = class {
   constructor(server) {
     this.server = server;
   }
 
   listen(port=3000, host='localhost', fn) {
-    this.server.listen(port, host, fn)
+    this.server.listen(port, host, fn);
+    debug('server is listening');
   }
 }
 
