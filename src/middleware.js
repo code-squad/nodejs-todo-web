ed6f8ed7e0bd7c12ffc9375d68c849c891abd580
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-18 21:22:34 +0900

    Feat : 미들웨어 실행 함수 추가
    미들웨어 배열에 등록된 미들웨어 차례데로 실행

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,33 @@
 const Middleware = class {
-  constructor() {
+  constructor(req, res) {
+    this.req = req;
+    this.res = res;
     this.middlewareArr = [];
   }
 
   add(func) {
     this.middlewareArr.push(func);
+  }
+
+  run(req, res) {
+    this.req = req;
+    this.res = res;
+
+    this.middlewareRun(0);
+  }
+
+  isOverLength(index) {
+    return index >= this.middlewareArr.length || index < 0;
+  }
+
+  middlewareRun(index) {
+    if (this.isOverLength(index)) {
+      return;
+    }
+
+    const targetFuc = this.middlewareArr[index];
+    const next = () => this.middlewareRun(index + 1);
+    targetFuc(this.req, this.res, next);
   }
 }
 
