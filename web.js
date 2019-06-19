aa9b23d1d56e6b90d91f4a9db2ac8cd34a4f44da
Kong Son Park <sonypark0204@gmail.com>
2019-06-19 17:25:54 +0900

    [Sony] step9-2 Todo App Webpage (#6)
    * [feat] Create todo html file
    
    * [feat] Create css file
    
    * [feat] Create web.js file
    
    - 새로운 todo 아이템 추가 메서드 생성
    
    * [feat] todo 아이템 추가, 수정, 삭제 메서드 생성
    
    - 기본 틀만 작성
    - 하나씩 기능 추가할 예정
    
    * [feat] item 을 todo, doing, done 리스트에 추가하는 메서드 생성
    
    - 일단은 checkbox 를 이용한 방식으로 구현할 예정
    - checkbox를 누르면 다음 리스트로 이동
    - todo list -> doing list -> done list 순서
    - done list 에서 checkbox를 누르면 다시 todo list로 이동
    
    * [feat] 버튼에 이벤트 바인딩하는 메서드 생성
    
    - listItem element에 버튼 이벤트 바인딩
    - 체크박스, 수정, 삭제 버튼
    
    * [feat] 각 버튼에 버튼 이벤트 바인딩
    
    * [test] 버튼 이벤트 동작 테스트
    
    * [feat] Drag and drop 이벤트 구현
    
    - addButton에 event 추가
    
    * [feat] 빈 리스트에 item 드래그 할 수 있도록 구현
    
    - 기존에는 이미 item이 있는 리스트에만 드래그로 이동이 가능했다.
    - 기존 아이템의 위 아래에 삽입만 가능했는데 빈 리스트에도 드래그하여 추가하도록 수정했다.
    
    * [feat] Enter 키로 항목 추가 기능 구현
    
    - 기존에는 todo 항목을 추가하려면 Add 버튼을 마우스로 클릭해야했다.
    - Enter 키만 쳐도 추가가 가능하도록 수정했다.
    
    * [refactor] 기존 기능의 사용성 업그레이드
    
    #1. Edit 기능
    - Edit 버튼을 누르면 아래에 Edit input 상자를 새로 생성하지 않고 기존 텍스트 위치에서 바로 편집 할 수 있도록 수정
    
    - Enter 이벤트를 추가하여 save 버튼을 클릭하지 않고 엔터키만으로도 save 할 수 있도록 구현
    
    #2. 스크롤 기능
    - 각 리스트에 추가된 목록이 일정 길이 이상이 되면 리스트 옆에 스크롤이 생기도록 구현
    
    #3. 체크박스 표시 수정
    - completed list 에 있는 목록에만 체크 표시가 되도록 수정

---
+++ web.js
@@ -0,0 +1,225 @@
+const taskInput = document.getElementById("new-task");
+const incompleteTaskHolder = document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
+const completedTasksHolder = document.getElementById("completed-tasks"); //ul of #completed-tasks
+const inProgressTaskHolder = document.getElementById('inprogress-tasks'); //ul of #inprogress-tasks
+
+
+//New task list item
+const createNewTaskElement = function (newTask) {
+
+    const listItem = document.createElement("li");
+    const checkBox = document.createElement("input");
+    const label = document.createElement("label");
+    const editInput = document.createElement("input");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    label.innerText = newTask;
+
+    checkBox.type = "checkbox";
+    editInput.type = "text";
+    editButton.innerText = "Edit";
+    editButton.className = "edit";
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete";
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+    listItem.draggable = true;
+    addEnterKeyEvent(editInput, editTask);
+    addDragEvent(listItem);
+    return listItem;
+};
+
+const addTask = function () {
+    console.log("Add Task...");
+    const listItem = createNewTaskElement(taskInput.value);
+
+    if (taskInput.value === '') {
+        window.alert('추가할 항목을 입력해주세요');
+    } else {
+        incompleteTaskHolder.appendChild(listItem);
+        bindTaskEvents(listItem, taskInProgress);
+        taskInput.value = "";
+    }
+
+};
+
+const editTask = function (event) {
+    console.log("Edit Task...");
+    const listItem = event.target.parentNode;
+
+    const todoTask = listItem.querySelector("label");
+    const editInput = listItem.querySelector('input[type=text]');
+    const containsClass = listItem.classList.contains("editMode");
+    const delete_button = listItem.querySelector('button.delete');
+
+    //If class of the parent is .editmode
+    if (containsClass) {
+        todoTask.innerText = editInput.value;
+        todoTask.hidden = false;
+        event.target.innerText = 'Edit';
+        delete_button.hidden = false;
+
+    } else {
+        editInput.value = todoTask.innerText;
+        todoTask.hidden = true;
+        event.target.innerText = 'save';
+        delete_button.hidden = true;
+    }
+    //toggle .editmode on the parent.
+    listItem.classList.toggle("editMode");
+    editInput.focus();
+};
+
+
+const deleteTask = function () {
+    console.log("Delete Task...");
+    const listItem = this.parentNode;
+    const ul = listItem.parentNode;
+    ul.removeChild(listItem);
+};
+
+const taskCompleted = function (event) {
+    console.log("Complete Task...");
+    event.target.checked = true;
+    const listItem = this.parentNode;
+    completedTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskIncomplete);
+};
+
+const taskIncomplete = function (event) {
+    console.log("Incomplete Task...");
+    event.target.checked = false;
+    const listItem = this.parentNode;
+    incompleteTaskHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskInProgress);
+};
+
+const taskInProgress = function (event) {
+    console.log("in Progress Task...");
+    event.target.checked = false;
+    const listItem = this.parentNode;
+    inProgressTaskHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+};
+
+const bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
+    console.log("bind list item events");
+    const checkBox = taskListItem.querySelector("input[type=checkbox]");
+    const editButton = taskListItem.querySelector("button.edit");
+    const deleteButton = taskListItem.querySelector("button.delete");
+
+    editButton.onclick = editTask;
+    deleteButton.onclick = deleteTask;
+    checkBox.onchange = checkBoxEventHandler;
+};
+
+const bindEventOnExistingItem = function (taskHolder, checkBoxEvent) {
+    Object.keys(taskHolder.children).forEach(v => {
+        bindTaskEvents(taskHolder.children[v], checkBoxEvent);
+        addDragEvent(taskHolder.children[v])
+    });
+};
+
+const bindDragEventOnList = function () {
+    // Bind event on addButton
+    const addButton = document.getElementsByTagName("button")[0];
+    addButton.onclick = addTask;
+    addEnterKeyEvent(taskInput, addTask);
+
+    const todoList_section = document.getElementById('todo-list');
+    const doingList_section = document.getElementById('doing-list');
+    const doneList_section = document.getElementById('done-list');
+
+    const todo_editInput = todoList_section.querySelector('input[type=text]');
+    const doing_editInput = doingList_section.querySelector('input[type=text]');
+    const done_editInput = doneList_section.querySelector('input[type=text]');
+
+    addEnterKeyEvent(todo_editInput, editTask);
+    addEnterKeyEvent(doing_editInput, editTask);
+    addEnterKeyEvent(done_editInput, editTask);
+
+    addDragEvent(todoList_section);
+    addDragEvent(doingList_section);
+    addDragEvent(doneList_section);
+
+    bindEventOnExistingItem(incompleteTaskHolder, taskInProgress);
+    bindEventOnExistingItem(inProgressTaskHolder, taskCompleted);
+    bindEventOnExistingItem(completedTasksHolder, taskIncomplete);
+
+};
+
+const addEnterKeyEvent = function (taskInput, eventHandler) {
+    taskInput.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13) {
+            eventHandler(event)
+        }
+    })
+};
+
+
+let draggingTarget = null;
+
+const addDragEvent = function (listItem) {
+
+    listItem.addEventListener('dragstart', function (event) {
+        draggingTarget = getDragTarget(event.target);
+        event.dataTransfer.setData('text/plain', null);
+        event.dataTransfer.setDragImage(draggingTarget, 0, 0);
+    });
+
+    listItem.addEventListener('dragover', function (event) {
+        event.preventDefault();
+        const target = getDragTarget(event.target);
+        const bounding = target.getBoundingClientRect();
+        const offset = bounding.y + (bounding.height / 2);
+
+        if (event.clientY - offset > 0) {
+            target.style['border-bottom'] = 'solid 4px black';
+            target.style['border-top'] = '';
+        } else {
+            target.style['border-top'] = 'solid 4px black';
+            target.style['border-bottom'] = '';
+        }
+    });
+
+    listItem.addEventListener('dragleave', function (event) {
+        const target = getDragTarget(event.target);
+        target.style['border-bottom'] = '';
+        target.style['border-top'] = '';
+    });
+
+    listItem.addEventListener('drop', function (event) {
+        event.preventDefault();
+        const target = getDragTarget(event.target);
+        if (target.children.length === 2) {
+            target.style['border-bottom'] = '';
+            target.style['border-top'] = '';
+            target.children[1].appendChild(draggingTarget)
+        } else if (target.style['border-bottom'] !== '') {
+            target.style['border-bottom'] = '';
+            target.parentNode.insertBefore(draggingTarget, event.target.nextSibling);
+        } else {
+            target.style['border-top'] = '';
+            target.parentNode.insertBefore(draggingTarget, event.target);
+        }
+    });
+
+    function getDragTarget(target) {
+        while (target.nodeName.toLowerCase() !== 'div' && target.nodeName.toLowerCase() !== 'li' && target.nodeName.toLowerCase() !== 'body') {
+            target = target.parentNode;
+        }
+        if (target.nodeName.toLowerCase() === 'body') return false;
+        else if (target.nodeName.toLowerCase() === 'div') return target;
+        else return target;
+    }
+};
+
+const init = function () {
+    bindDragEventOnList();
+};
+init();
