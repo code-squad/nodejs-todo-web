adab862e2732472ab4cf1a93808db45804b7f2c9
LeeJuyeon <ruby413@hanmail.net>
2019-06-19 11:22:00 +0900

    refactor : refactoring

--- event.js
+++ event.js
@@ -1,4 +1,3 @@
-
 const display = (displayID, displayNoneID) => {
     document.getElementById(displayID).style.display="inherit";
     document.getElementById(displayNoneID).style.display="none";
@@ -10,7 +9,6 @@ const callAppendChild = (targetId, valueId) => {
     const targetUl = document.getElementById(targetId); 
     const li = document.createElement('li'); 
     const span = document.createElement('span'); 
-    let dragging = null;
 
     if(data !== ""){
         li.classList.add("contents")
@@ -20,35 +18,37 @@ const callAppendChild = (targetId, valueId) => {
         span.classList.add("icon-hamburger-menu-close")
         targetUl.appendChild(li); 
         document.getElementById(valueId).value = "";   
-        
-        li.setAttribute('draggable', true); 
         li.addEventListener("mouseover", ()=>{span.style.display = "block";}, false)
         li.addEventListener("mouseout", ()=>{span.style.display = "none";}, false)
         span.addEventListener("click", (ev)=>{ev.target.parentNode.parentNode.removeChild(ev.target.parentNode);}, false)
 
-
-        document.addEventListener('dragstart', (event) => { 
-            dragging = event.target;
-            event.dataTransfer.setData('text/html', dragging);
-        });
-
-        document.addEventListener('dragover', (event) => {
-            event.preventDefault();
-        });
-
-        targetUl.addEventListener('dragenter', (event) => {
-            event.target.style['border-bottom'] = 'solid 2px blue';
-        });
-
-        targetUl.addEventListener('dragleave', (event) => {
-            event.target.style['border-bottom'] = '';
-        });
-
-        targetUl.addEventListener('drop', (event) => {
-            event.preventDefault();
-            event.target.style['border-bottom'] = '';
-            event.target.parentNode.insertBefore(dragging, event.target.nextSibling);
-        });
-
+        drag(targetUl,li)
     }
+}
+
+const drag = (listWrap, list) => {
+    let dragging = null;
+    list.setAttribute('draggable', true); 
+    document.addEventListener('dragstart', (event) => { 
+        dragging = event.target;
+        event.dataTransfer.setData('text/html', dragging);
+    });
+
+    document.addEventListener('dragover', (event) => {
+        event.preventDefault();
+    });
+
+    listWrap.addEventListener('dragenter', (event) => {
+        event.target.style['border-bottom'] = 'solid 2px blue';
+    });
+
+    listWrap.addEventListener('dragleave', (event) => {
+        event.target.style['border-bottom'] = '';
+    });
+
+    listWrap.addEventListener('drop', (event) => {
+        event.preventDefault();
+        event.target.style['border-bottom'] = '';
+        event.target.parentNode.insertBefore(dragging, event.target.nextSibling);
+    });
 }
