691cc7391a313c50ed9b2be9246786b995957315
hsycamp <hsycamp@gmail.com>
2019-06-19 21:25:51 +0900

    feat : todo card를 생성하는 메소드 작성

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const createNewCard = function(input) {
+  const newCard = document.createElement("div");
+  const cardText = document.createTextNode(input);
+  newCard.appendChild(cardText);
+  return newCard;
+};
+
 const createEvent = function() {
   const addButton = document.getElementsByClassName("add-card-btn")[0];
   addButton.addEventListener("click", function(event) {
