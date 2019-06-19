95ad02adce1d456215012b2b272e0758e1a7999f
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-19 16:41:05 +0900

    [feat] HTTP 상태 코드를 관리하는 stateCode 객체 생성

---
+++ state_code.js
@@ -0,0 +1,15 @@
+module.exports.stateCode = {
+    // Success
+    'OK'                    : 200,
+    'CREATED'               : 201,
+    // Redirection
+    'MOVED_PERMANENTLY'     : 301,
+    'FOUND'                 : 302,
+    'NOT_MODIFIED'          : 304,
+    // Client Error
+    'BAD_REQUEST'           : 400,
+    'FORBIDDEN'             : 403,
+    'NOT_FOUND'             : 404,
+    'METHOD_NOT_ALLOWED'    : 405,
+    'TOO_MANY_REQUESTS'     : 429,
+}
