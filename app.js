d081a8897eba75fc2f5f53f24150c7e645716c11
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-19 19:20:29 +0900

    Refactor : index 함수 app.js로 위치 변경,

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 const App = require('./src/application');
 const app = new App();
+const ServeStatic = require('./middlewares/serve-static');
+const serveStatic = new ServeStatic(app.req, app.res);
+
+const path = require('path')
+const fs = require('fs')
+
+const index = (req, res, next) => {
+  const publicPath = path.join(__dirname, './public')
+
+  fs.readFile(`${publicPath}/index.html`, (err, data) => {
+    if (err) throw err
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+  })
+}
+
+app.use(index);
+app.use(serveStatic.serveStaticFile);
 
 module.exports = app;
