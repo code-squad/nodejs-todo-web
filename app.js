c8d248bd738c484d7d31ca79f30f8198090c5c35
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-20 20:04:41 +0900

    Create : 정적 페이지를 받아오는 controller 생성
    login, todo 페이지 받아오기

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = new App();
 const serveStatic = require('./middlewares/serve-static');
 const errorHandler = require('./middlewares/errorHandler');
 const logger = require('./middlewares/logger');
-const index = require('./routers/index');
+const todoController = require('./controller/todo');
+const loginController = require('./controller/login');
 
 app.use(logger());
 app.use(serveStatic());
-app.use('/', index.index());
+app.use('/', loginController.getPage());
+app.use('/todo', todoController.getPage());
 app.use(errorHandler.error404());
 app.use(errorHandler.error500());
 
---
+++ controller/login.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+const getPage = () => (req, res, next) => {
+  const publicPath = path.join(__dirname, '../public')
+
+  fs.readFile(`${publicPath}/login.html`, (err, data) => {
+    if (err) throw err
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+  })
+}
+
+module.exports = {
+  getPage,
+}
