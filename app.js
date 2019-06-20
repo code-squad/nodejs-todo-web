6033fd301d8a819ddfb446a744cf9c61771f59f4
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-20 14:51:18 +0900

    Refactor : 요청 url에 따라 index파일을 가져오는 기능 router로 분리

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 const App = require('./src/application');
 const app = new App();
-const ServeStatic = require('./middlewares/serve-static');
-const serveStatic = new ServeStatic(app.req, app.res);
-
-const path = require('path')
-const fs = require('fs')
-
-const index = (req, res, next) => {
-  const publicPath = path.join(__dirname, './public')
-
-  fs.readFile(`${publicPath}/index.html`, (err, data) => {
-    if (err) throw err
-
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end(data)
-  })
-}
+const serveStatic = require('./middlewares/serve-static');
+const index = require('./routers/index')
 
 const error404 = (req, res, next) => {
   res.statusCode = 404;
@@ -28,8 +13,8 @@ const error = (err, req, res, next) => {
   res.end()
 }
 
-app.use(index);
-app.use(serveStatic.serveStaticFile);
+app.use('/', index.index());
+app.use(serveStatic());
 app.use(error404);
 app.use(error);
 
