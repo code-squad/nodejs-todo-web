aa1a35e139eeb8ce40c5893afd7edc79ecf75a85
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-20 17:07:01 +0900

    Refactor : 404, 500 에러 처리 모듈 분리
    에러 처리 모듈 분리 후 미들웨어 등록

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const App = require('./src/application');
 const app = new App();
 const serveStatic = require('./middlewares/serve-static');
+const errorHandler = require('./middlewares/errorHandler');
 const logger = require('./middlewares/logger');
 const index = require('./routers/index');
 
-const error404 = (req, res, next) => {
-  res.statusCode = 404;
-  res.end('Not Found')
-}
-
-const error = (err, req, res, next) => {
-  res.statusCode = 500;
-  res.end()
-}
-
 app.use(logger());
-app.use('/', index.index());
 app.use(serveStatic());
-app.use(error404);
-app.use(error);
+app.use('/', index.index());
+app.use(errorHandler.error404());
+app.use(errorHandler.error500());
 
 module.exports = app;
