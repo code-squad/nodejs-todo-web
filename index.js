41e41e1209512972d91a38e6b9ef0cbc5d934a2c
hsycamp <hsycamp@gmail.com>
2019-06-21 01:47:18 +0900

    feat : drag 이벤트 메소드 작성
    각 리스트 내부에서 card들이 이동할 수 있도록하는 메소드 구현

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
+let draggingTarget = null;
+
 const createNewCard = function(input) {
   const newCard = document.createElement("div");
   const cardText = document.createTextNode(input);
+  newCard.setAttribute("class", "todo-card");
+  newCard.setAttribute("draggable", true);
   newCard.appendChild(cardText);
+  addDragEvent(newCard);
   return newCard;
 };
 
 const addNewCard = function() {
-  const inputText = document.getElementById("todo-input-text");
-  const todoList = document.querySelector(".todo-board");
+  const inputText = document.querySelector("#todo-input-text");
+  const todoList = document.querySelector(".todo-list");
+  const todoBoard = todoList.querySelector(".board");
   const newCard = createNewCard(inputText.value);
-  newCard.setAttribute("class", "todo-card");
-  todoList.appendChild(newCard);
+  todoBoard.appendChild(newCard);
   inputText.value = "";
   hideDiv();
 };
 
 const createEvent = function() {
-  const addButton = document.getElementsByClassName("add-card-btn")[0];
+  const addButton = document.querySelector("#add-input-btn");
   addButton.addEventListener("click", function(event) {
     showDiv();
   });
-  const inputText = document.getElementById("todo-input-text");
+
+  const inputText = document.querySelector("#todo-input-text");
   createEnterKeyEvent(inputText, addNewCard);
 };
 
@@ -33,14 +39,48 @@ const createEnterKeyEvent = function(inputText, eventHandler) {
 };
 
 const showDiv = function() {
-  const target = document.getElementById("todo-input-text");
+  const target = document.querySelector("#todo-input-text");
   target.style.display = "block";
   target.focus();
 };
 
 const hideDiv = function() {
-  const target = document.getElementById("todo-input-text");
+  const target = document.querySelector("#todo-input-text");
   target.style.display = "none";
+};
+
+const addDragEvent = function(card) {
+  card.addEventListener("dragstart", function(event) {
+    draggingTarget = event.target;
+    event.dataTransfer.setData("text/html", draggingTarget);
+  });
+
+  card.addEventListener("dragover", function(event) {
+    event.preventDefault();
+  });
+
+  card.addEventListener("dragenter", function(event) {
+    if (!event.target.nextElementSibling) {
+      event.target.style["border-bottom"] = "solid 7px darkblue";
+    } else {
+      event.target.style["border-top"] = "solid 7px darkblue";
+    }
+  });
+
+  card.addEventListener("dragleave", function(event) {
+    event.target.style["border-bottom"] = "";
+    event.target.style["border-top"] = "";
+  });
+
+  card.addEventListener("drop", function(event) {
+    event.target.style["border-top"] = "";
+    event.target.style["border-bottom"] = "";
+    if (!event.target.nextElementSibling) {
+      event.target.parentNode.appendChild(draggingTarget);
+    } else {
+      event.target.parentNode.insertBefore(draggingTarget, event.target);
+    }
+  });
 };
 
 createEvent();
