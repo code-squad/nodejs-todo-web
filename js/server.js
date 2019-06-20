d6d1cdc7a24fe2fd98d09920e92baac85f803565
mukeunzi <eunji057@gmail.com>
2019-06-21 03:37:24 +0900

    feat : reqest, response 변경
    - 요청이 들어오면 공통적으로 pathname, method, ext 를 구함

--- js/server.js
+++ js/server.js
@@ -1,11 +1,20 @@
 const http = require('http');
 const path = require('path');
+const url = require('url');
 const fs = require('fs');
 
 const port = 8000;
 
 const server = http.createServer((req, res) => {
-	if (req.url === '/' && req.method === 'GET') {
+	const pathname = url.parse(req.url).pathname;
+	const method = req.method;
+	const ext = path.parse(req.url).ext;
+
+	if (!ext) {
+		ext = '.html';
+	}
+
+	if (pathname === '/' && method === 'GET') {
 		fs.readFile('../index.html', (error, file) => {
 			if (error) {
 				throw new Error('파일로드오류');
