9f36fb1a7ef637ea47415384861e7742c2d568b7
Ruby Lee <48516485+ruby413@users.noreply.github.com>
2019-06-20 16:12:25 +0900

    [ruby] step9-2 TodoWep 구현 보내드립니다. (#7)
    * create : index.html, css, js
    
    * create : make addlist button
    
    * create : make doing and done
    
    * refactor : refactor
    ing
    
    * create : drag and drop
    
    * fixed : deletebutton
    
    * fixed : delete method
    
    * refactor : refactoring

---
+++ event.js
@@ -0,0 +1,54 @@
+const display = (displayID, displayNoneID) => {
+    document.getElementById(displayID).style.display="inherit";
+    document.getElementById(displayNoneID).style.display="none";
+}
+
+const callAppendChild = (targetId, valueId) => { 
+    const data = document.getElementById(valueId).value;
+    const text = document.createTextNode(data); 
+    const targetUl = document.getElementById(targetId); 
+    const li = document.createElement('li'); 
+    const span = document.createElement('span'); 
+
+    if(data !== ""){
+        li.classList.add("contents")
+        li.appendChild(text); 
+        li.appendChild(span); 
+        span.classList.add("deleteData")
+        span.classList.add("icon-hamburger-menu-close")
+        targetUl.appendChild(li); 
+        document.getElementById(valueId).value = "";   
+        li.addEventListener("mouseover", ()=>{span.style.display = "block";}, false)
+        li.addEventListener("mouseout", ()=>{span.style.display = "none";}, false)
+        span.addEventListener("click", (ev)=>{ev.target.parentNode.parentNode.removeChild(ev.target.parentNode);}, false)
+
+        drag(targetUl,li)
+    }
+}
+
+const drag = (listWrap, list) => {
+    let dragging = null;
+    list.setAttribute('draggable', true); 
+    document.addEventListener('dragstart', (event) => { 
+        dragging = event.target;
+        event.dataTransfer.setData('text/html', dragging);
+    });
+
+    document.addEventListener('dragover', (event) => {
+        event.preventDefault();
+    });
+
+    listWrap.addEventListener('dragenter', (event) => {
+        event.target.style['border-bottom'] = 'solid 2px blue';
+    });
+
+    listWrap.addEventListener('dragleave', (event) => {
+        event.target.style['border-bottom'] = '';
+    });
+
+    listWrap.addEventListener('drop', (event) => {
+        event.preventDefault();
+        event.target.style['border-bottom'] = '';
+        event.target.parentNode.insertBefore(dragging, event.target.nextSibling);
+    });
+}
