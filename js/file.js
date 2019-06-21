3537440d640c45f919cbc60c9bf0c54174a4c626
mukeunzi <eunji057@gmail.com>
2019-06-21 20:22:14 +0900

    feat : readFile 메소드 생성
    - 파일을 불러오는 메소드

--- js/file.js
+++ js/file.js
@@ -1,7 +1,25 @@
+const fs = require('fs');
 const mimeTypeList = require('../data/mime-type.js');
+
+const readFile = (filePath, ext) => {
+	return new Promise((resolve, reject) => {
+		const mimeType = getMimeType(ext);
+		fs.readFile(filePath, (error, file) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve({ file, mimeType });
+			}
+		});
+	});
+};
 
 const getMimeType = ext => {
 	if (Object.keys(mimeTypeList).includes(ext)) {
 		return mimeTypeList[ext];
 	}
+};
+
+module.exports = {
+	readFile
 };
