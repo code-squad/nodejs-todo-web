576d31954ad8bceeb68f28e15a1ddaa291c01905
LeeJuyeon <ruby413@hanmail.net>
2019-06-21 12:11:09 +0900

    feat : make cookies in client - login_process : set cookies

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
 const http = require('http');
 const url = require('url');
 const loginTemplate = require('./login_template');
-var qs = require('querystring');
+const qs = require('querystring');
 const Login = require('./login');
-const login = new Login
-
-// let template = `<form action="/signup_process" method="post">
-//                 <p><span>닉네임 </span><input type="text" name="nickName" placeholder="nickName"></p>
-//                 <p><span>이메일 </span><input type="text" name="email" placeholder="email"></p>
-//                 <p><span>비밀번호 </span><input type="password" name="pwd" placeholder="password"></p>
-//                 <p><span>비밀번호 확인 </span><input type="password" name="check_pwd" placeholder="password"></p>
-//                 <p>
-//                 <input type="submit" value="signUp">
-//                 </p>
-//                 </form>`
+const login = new Login;
 
 const app = http.createServer( function(request,response){
     let _url = request.url;
@@ -42,22 +32,33 @@ const app = http.createServer( function(request,response){
         </form>`
         response.end(loginTemplate.HTML(template));
     }else if(url.parse(_url, true).pathname === "/login_process"){
-
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', async function(){
+            var post = qs.parse(body);
+            let loginCheck = await login.checkLogin(post);
+            let nickName = await login.findNickname(post.email);
+            if(loginCheck) {
+              response.writeHead(302, {
+                'Set-Cookie':[
+                  `email=${post.email}`,
+                  `password=${post.pwd}`,
+                  `nickname=${nickName}`
+                ],
+                Location: `/`
+              });
+            }
+            response.end();
+        });
     }else if(url.parse(_url, true).pathname === "/signup_process"){
         let body = '';
         request.on('data', function (data) {
             body += data;
         });
-        // alert(body)
-        // login.signUp()
         request.on('end',async function () {
             let post = qs.parse(body);
-        //     var title = post.title;
-        //     var description = post.description;
-        //     fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-        //       response.writeHead(302, {Location: `/?id=${title}`}); //302 - 강제이동
-        //       response.end();
-        //   }); 
             let signUp = await login.signUp(post)
             let error = "<p>이미 이메일주소가 있는 가입정보입니다.</p>"
             console.log(signUp)
@@ -66,9 +67,10 @@ const app = http.createServer( function(request,response){
                 response.writeHead(302, {Location: `/`}); 
                 response.end();
             }else{
-                console.log("dd")
-                response.writeHead(302, {Location: `/signUp`}); 
-                response.end(loginTemplate.HTML(error+this.outerHTML));
+                setTimeout(function() {
+                    response.writeHead(302, {Location: `/signUp`}); 
+                    response.end();
+                }, 2000);
             }
         });
     }
