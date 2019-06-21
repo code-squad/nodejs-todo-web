79f69ff6cb09ff522bc4c6da5989da652fe93731
mukeunzi <eunji057@gmail.com>
2019-06-21 20:20:09 +0900

    feat : getMimeType 메소드 생성
    - 파라미터의 값에 따라  mimeType을 리턴하는 메소드

--- js/file.js
+++ js/file.js
@@ -1,15 +1,7 @@
-const fs = require('fs');
+const mimeTypeList = require('../data/mime-type.js');
 
-const readFile = filePath => {
-	fs.readFile(filePath, (error, file) => {
-		if (error) {
-			console.log(error);
-			res.writeHead(404, { 'Content-Type': 'text/plain' });
-			res.write('404 file not found!');
-		} else {
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.write(file);
-		}
-		res.end();
-	});
+const getMimeType = ext => {
+	if (Object.keys(mimeTypeList).includes(ext)) {
+		return mimeTypeList[ext];
+	}
 };
