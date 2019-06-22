25f97b41d5d8c03e735cd7ee91103619cbfa32fe
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-22 18:13:08 +0900

    Feat : session 미들웨어 등록

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const logger = require('./middlewares/logger');
 const bodyParser = require('./middlewares/body-parser');
 const todoController = require('./controller/todo');
 const loginController = require('./controller/login');
+const session = require('./middlewares/session');
 
 app.use(logger());
+app.use(session());
 app.use(serveStatic());
 app.use(bodyParser());
 app.get('/', loginController.getPage());
