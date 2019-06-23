944272d683f97845ca214f130cd217dd0271a67b
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-23 18:02:19 +0900

    Feat : 뷰 리졸버 사용해 동적으로 html 파일 생성
    카드 정보 생성 후 뷰 리졸버에 data 위임

--- controller/todo.js
+++ controller/todo.js
@@ -1,16 +1,54 @@
-const path = require('path');
-const fs = require('fs');
+const csvParser = require('../utils/csv-parser');
+const view = require('../utils/viewResolver');
 
-const getPage = () => (req, res, next) => {
-  const publicPath = path.join(__dirname, '../public')
+const createCard = (dataOBj) => {
+  return Object.keys(dataOBj).reduce((acc, contentType) => {
+    if (dataOBj[contentType] === '') {
+      return acc;
+    }
+    const htmlString = dataOBj[contentType].split('&').reduce((acc,content) => {
+      const string = `<section class="card ${contentType}" draggable="true"><img src="img/exit.png" alt="exit-image" class="card-image-exit">${content}</section>`
+      acc += string;
+      return acc;
+    },'');
 
-  fs.readFile(`${publicPath}/index.html`, (err, data) => {
-    if (err) throw err
+    acc[contentType] = htmlString;
+    return acc;
+  }, {});
+}
 
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end(data)
-  })
+const getCard = (cardObj, type) => {
+  return cardObj.hasOwnProperty(type) ? cardObj[type] : null
+};
+
+const getPage = () => async (req, res, next) => {
+
+  if (req.session === 'false') {
+    res.writeHead(302, {'Location': '/' });
+    res.end();
+    return;
+  }
+
+  const todoData = await csvParser.getKeyValueObj('./db/todoList.csv');
+  const userID = req.session.userID;
+
+  const dataOBj = {
+    'userID' : userID,
+    'todoData' : todoData[userID]
+  }
+
+  const cardObj = createCard(dataOBj.todoData);
+  const viewResolverObj = {
+    'userID' : userID,
+    'todo' : getCard(cardObj, 'todo'),
+    'doing' : getCard(cardObj, 'doing'),
+    'done' : getCard(cardObj, 'done'),
+  }
+
+  const viewer = await view(viewResolverObj);
+  res.writeHead = (200, {'Content-Type' : 'text/html'}),
+  res.write(viewer);
+  res.end();
 }
 
 module.exports = {
