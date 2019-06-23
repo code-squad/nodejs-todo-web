6280956413a82bbd2fcdd01d337af1f6814e5afa
MCC <cocahack@naver.com>
2019-06-23 16:07:08 +0900

    feat: Todo, Todolist 모델 추가
    Todo와 Todolist를 표현하는 클래스 추가

---
+++ model/todo.js
@@ -0,0 +1,10 @@
+class Todo {
+  constructor(id, name, position, todoListName) {
+    this.id = id;
+    this.name = name;
+    this.position = position;
+    this.todoListName = todoListName;
+  }
+}
+
+module.exports = Todo;
---
+++ model/todolist.js
@@ -0,0 +1,9 @@
+class TodoList {
+  constructor(id, name, position){
+    this.id = id;
+    this.name = name;
+    this.position = position;
+  }
+}
+
+module.exports = TodoList;
