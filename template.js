f3b5b11fb95a3afe087f7b9a9e46623b36a64b83
MCC <cocahack@naver.com>
2019-06-23 15:46:59 +0900

    feat: template.js 추가
    html 코드를 생성하는 파일
    TODO 데이터에 따라 동적으로 코드를 생성함

---
+++ template.js
@@ -0,0 +1,64 @@
+function makeTodoHtmlText(todo) {
+  return `<li class="collection-item todo-item" draggable="true"><div>${todo.name}</div></li>`
+}
+
+function makeTodoListHtmlText(todoList, todos) { 
+  const todosHtml = todos.map(todo => makeTodoHtmlText(todo)).join('\n');
+  return `<div class="todo-column todolist-container grey lighten-2">
+<div class="todolist-header">
+  ${todoList}
+</div>
+<ul class="collection">
+  ${todosHtml}
+</ul>
+<div class="todolist-footer">
+  <button class="add-card">Add card</button>
+  <div class="add-card-form hide-element">
+    <input class="input-field new-card-name" placeholder="Input new card name">
+  </div>
+</div>
+</div>`;
+}
+
+function makeIndexHtmlText(todoLists, todoSets) {
+  const todoListsHtmlText = todoLists.map(todoList => makeTodoListHtmlText(todoList.name, todoSets[todoList.name])).join('\n');
+  return `<!DOCTYPE html>
+  <html lang="ko">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="public/index.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="public/materialize.min.css" media="screen" />
+    <title>TODO</title>
+  </head>
+  <body>
+    <div class="surface">
+      <div class="header white-text grey darken-3">
+        Todo
+      </div>
+      <div class="shadow-header deep-orange-text">
+          This element wiil not shown.
+      </div>
+      <div id="stage">
+        ${todoListsHtmlText}
+        <div class="todo-container">
+
+          <div class="todo-column">
+            <button id="add-todo-btn" class="btn-large grey">Add list</button>
+            <div class="add-list-form hide-element">
+              <input type="text" class="input-field new-list-name" placeholder="Input new todolist name">
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="background"></div>
+  </body>
+  <script src="public/index.js"></script>
+  </html>`
+}
+
+module.exports = makeIndexHtmlText;
