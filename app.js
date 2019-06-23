96bfebb42fa93deb8dd9eb838ce7d8f8611a9f54
MCC <cocahack@naver.com>
2019-06-23 22:34:53 +0900

    feat: todolist POST 라우터 추가
    todolist 추가 시 실행될 라우터 추가

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const http = require('http');
 const fs = require('fs');
 const makeIndexHtmlText = require('./template');
 const { getFileExtentsion, makeFilePath, getMimeType } = require('./util');
+const db = require('./db');
 
-let todoSets;
+const Todo = require('./model/todo');
+const TodoList = require('./model/todolist');
+
+let todos;
 let todoLists;
 
 const server = http.createServer(async (request, response) => {
   try {
     const result = await require('./db').read();
-    todoSets = result.todoSets;
+    todos = result.todos;
     todoLists = result.todoLists;
   } catch (error) {
     console.error(error);
@@ -29,17 +33,32 @@ const server = http.createServer(async (request, response) => {
 
   if (request.method === 'GET' && request.url === '/') {
     response.setHeader('Content-Type', 'text/html');
-    response.write(makeIndexHtmlText(todoLists, todoSets));
+    response.write(makeIndexHtmlText(todoLists, todos));
     response.end();
   } else if (request.method === 'GET' && RegExp(/\/public/).test(request.url)) {
     response.setHeader('Content-Type', getMimeType(getFileExtentsion(request.url)));
     response.write(fs.readFileSync(makeFilePath(request.url)));
     response.end();
-  } else if (request.method === 'POST' && RegExp(/\/todo/).test(request.url)) {
-    // TODO:  todo create 로직
-    response.setHeader('Content-Type', 'application/json');
-    response.write({});
-    response.end();
+  } else if (request.method === 'POST' && RegExp(/\/todo$/).test(request.url)) {
+    let body, todo;
+    request.on('data', chunk => {
+      body = JSON.parse(chunk.toString('utf-8'));
+      todo = new Todo(todos.slice(-1)[0].id + 1, body.name, body.position, body.todolist );
+      todos.push(todo);
+      db.create('todo', todo);
+    }).on('end', () => {
+      response.end();
+    });
+  } else if (request.method === 'POST' && RegExp(/\/todolist$/).test(request.url)) {
+    let body, todoList;
+    request.on('data', chunk => {
+      body = JSON.parse(chunk.toString('utf-8'));
+      todoList = new TodoList(todoLists.slice(-1)[0].id + 1, body.name, body.position );
+      todoLists.push(todoList);
+      db.create('todolist', todoList);
+    }).on('end', () => {
+      response.end();
+    });
   } else if (request.method === 'PUT' && RegExp(/\/todo/).test(request.url)) {
     // TODO: todo 업데이트 로직
     response.setHeader('Content-Type', 'application/json');
