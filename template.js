3fc814850d0995d602bc9d171f7aa6253041d6ab
MCC <cocahack@naver.com>
2019-06-24 14:49:13 +0900

    refactor: template 수정
    todo li 요소에 data-id 속성 추가
    ajax 요청 시 todo object를 보내기 위함

--- template.js
+++ template.js
@@ -1,5 +1,5 @@
 function makeTodoHtmlText(todo) {
-  return `<li class="collection-item todo-item" draggable="true"><div>${todo.name}</div></li>`
+  return `<li class="collection-item todo-item" draggable="true" data-id="${todo.id}"><div>${todo.name}</div></li>`
 }
 
 function makeTodoListHtmlText(todoList, todos) { 
@@ -26,6 +26,9 @@ function makeIndexHtmlText(todoLists, todos) {
     return acc;
   },{});
   todos.forEach(todo => todoSets[todo.todoListName].push(todo));
+  for (const key in todoSets) {
+    todoSets[key].sort((a, b) => a.position - b.position);
+  }
   const todoListsHtmlText = todoLists.map(todoList => makeTodoListHtmlText(todoList.name, todoSets[todoList.name])).join('\n');
   return `<!DOCTYPE html>
   <html lang="ko">
