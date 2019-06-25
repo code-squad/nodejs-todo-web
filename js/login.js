ddd8b5e357c7a42ab60eec0b9395b807bfdc884c
mukeunzi <eunji057@gmail.com>
2019-06-25 21:34:54 +0900

    feat : isValidMember 메소드 수정
    - fetch API 사용
    - response.ok 에 따라 해당하는 페이지로 redirect

--- js/login.js
+++ js/login.js
@@ -9,10 +9,21 @@ const load = () => {
 };
 
 const isValidMember = async () => {
-	const user_id = document.querySelector('#id');
-	const user_password = document.querySelector('#password');
+	const user_id = document.querySelector('#id').value;
+	const user_password = document.querySelector('#password').value;
+	const loginData = { user_id, user_password };
 
-	const response = await fetch('/login', { method: 'POST', body: { user_id, user_password } });
+	try {
+		const response = await fetch('/login', { method: 'POST', body: JSON.stringify(loginData) });
+		if (response.ok) {
+			location.href = '/';
+		} else {
+			location.href = '/error-404';
+		}
+	} catch (error) {
+		console.log('error.....', error);
+		location.href = '/error-500';
+	}
 };
 
 load();
