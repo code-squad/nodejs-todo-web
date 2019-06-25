3f4049e53475acaf44958a0d9e259e8dd71909d8
mukeunzi <eunji057@gmail.com>
2019-06-25 17:02:45 +0900

    feat : makeSessionId 메소드 생성
    - 임의의 sessionId 를 생성하는 메소드

--- js/member.js
+++ js/member.js
@@ -4,3 +4,10 @@ const adapter = new FileSync('../data/member.json');
 const members = low(adapter);
 
 members.defaults({ members: [] }).write();
+
+const makeSessionId = () => {
+	const min = 10000000000000000;
+	const max = 99999999999999999;
+
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
