a3cf26b421b2d75faf1def63629675c6df963c76
Min-92 <0201.mj.lee@gmail.com>
2019-06-25 22:03:43 +0900

    feat : login test code 추가

--- test/user_manager.spec.js
+++ test/user_manager.spec.js
@@ -14,9 +14,25 @@ class User {
 
 describe('# user manager test', () => {
     describe('# sign up test', () => {
-        it('should return user data object', () => {
-            assert.equal(JSON.stringify(userManager.signUp('wangmin', 1234)), JSON.stringify(new User('wangmin', 1234)))
+        it('should return user data', () => {
+            userManager.createDataFile();
+            const userData = userManager.signUp('wangman', 1234);
+            assert.equal(JSON.stringify(userData), JSON.stringify(new User('wangman', 1234)));
+        });
+        it('should return false', () => {
+            assert.equal(userManager.signUp('wangman', 1234), false)
         });
     });
+    describe('# log in test',() => {
+        it('should return user data', () =>{
+            const userData = userManager.logIn('wangman',1234);
+            const data = JSON.parse(fs.readFileSync('./data/userData.txt').toString());
+            assert.equal(JSON.stringify(userData), JSON.stringify(data['wangman']) )
+        });
+        it('should return false', ()=>{
+            const userData = userManager.logIn('wangman',123);
+            assert.equal(userData,false);
+        })
+    })
 });
 
