b958a1036cb3bbc88bc73e17d1c04ef7aa328f4f
Min-92 <0201.mj.lee@gmail.com>
2019-06-25 20:54:15 +0900

    feat : 로그인 구현
    - add logIn method
    - add createDataFile method
    - add loadData method
    - add isMember method
    - add readUserData method

--- user_manager.js
+++ user_manager.js
@@ -5,94 +5,74 @@ module.exports = class UserManager {
     }
 
     logIn(id, pw) {
-        userdata 불러오기
-        userdata 리턴하기
-
-        return readUserData(id, pw)
+        return this.readUserData(id, pw);
     }
 
     signUp(id, pw) {
-        data 불러오기
-        id 있는지 확인
-        있으면 false 반환
+        // data 불러오기
+        // id 있는지 확인
+        // 있으면 false 반환
 
-        없으면 유저 객체 만들기
-        this.data 에 유저 넣기
-        data 업데이트하기
-        유저 반환하기
+        // 없으면 유저 객체 만들기
+        // this.data 에 유저 넣기
+        // data 업데이트하기
+        // 유저 반환하기
 
-        loadData()
-        if(isMember()){
-            return false
-        }else{
-            const tempInfo = new User(id,pw)
-            this.data.id = tempInfo
-            updateUserData()
-            return this.data.id
-        }
+        // loadData()
+        // if(isMember()){
+        //     return false
+        // }else{
+        //     const tempInfo = new User(id,pw)
+        //     this.data.id = tempInfo
+        //     updateUserData()
+        //     return this.data.id
+        // }
 
     }
 
     createDataFile(){
-        data폴더 있는지 확인
-        없으면 만들기
-        파일 만들기
-
-        if(!folder exist){
-            mkdir ./data
+        if(!this.fileSystem.existSync('./data')){
+            this.fileSystem.mkdir('./data');
         }
-        write file (./data/data.csv)
+        this.fileSystem.writeFileSync('./data/userData.csv', '{}', 'utf8');
     }   
 
     loadData(){
-        data 파일 있는지 확인하기
-        없으면 만들기
-        data 불러오기
-        data 리턴하기
-
-        if( !fileExist ){
-            this.createDataFile()
+        if(!this.fileSystem.existSync('./data/userData.csv')){
+            this.createDataFile();
         }
-        this.data = readfile data
-
+        this.data = JSON.parse(this.fileSystem.readFIleSync('./data/userData.csv').toString());
     }
 
     isMember(id, pw) {
-        data 로드하기
-        id,pw 조회하기
-        결과 리턴하기
-        
-        loadData()
+        this.loadData();
         if(id in this.data){
-            this.data.id.pw === pw
+            this.data.id.pw === pw;
             return this.data.id;
         }
         return false
     }
 
     readUserData(id, pw) {
-        id, pw 확인하기
-        userdata 리턴하기
-
-        return isMember(id, pw)
+        return this.isMember(id, pw);
     }
 
     writeData(){
-        data저장하기
+        // data저장하기
 
-        write file (data.csv, this.userdata);
+        // write file (data.csv, this.userdata);
     }
 
     updateUserData(id) {
-        현재 id정보 따로 저장하기
-        data 불러오기
-        data에 현재 정보 넣기
-        데이터 쓰기
+        // 현재 id정보 따로 저장하기
+        // data 불러오기
+        // data에 현재 정보 넣기
+        // 데이터 쓰기
 
-        const tempInfo = this.data.id
-        loadData()
-        this.data.id = tempInfo
-        writeData()
+        // const tempInfo = this.data.id
+        // loadData()
+        // this.data.id = tempInfo
+        // writeData()
 
     }
 }
