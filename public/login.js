2f48875f31a0dfbe07f4484650d1cfc03ba3911b
MCC <cocahack@naver.com>
2019-06-26 16:49:32 +0900

    refactor: 로그인 페이지 수정
    비동기 로그인 구현
    - fetch의 redirected가 true면 /todo로 이동
    - statusCode가 403이면 로그인 에러 출력
    - 그 외 상태 코드는 서버 오류 출력
    UI
    - 로그인 결과를 출력하는 toast 관련 코드 추가

--- public/login.js
+++ public/login.js
@@ -2,23 +2,36 @@ var loginBtn = document.getElementById('login-btn');
 var inputId = document.getElementById('userId');
 var inputPassword = document.getElementById('password');
 
-function fetchData(url = '/todo', data = {}, method = 'POST') {
+function fetchData(url = '/todo', data = {}) {
   return fetch(url, {
-    method,
-    headers: {
-      'Content-Type': 'application/json'
+    method: 'POST', 
+    redirect: 'follow',           
+    headers: { 
+      "Accept": "application/json , text/plain ,*/*",
+      "Content-type": "application/json"
     },
-    referrer: 'no-referrer',
     body: JSON.stringify(data)
   })
-  .then(response => response.json());
+  .then(response => response);
 }
 
 loginBtn.addEventListener('click', function(e){
   e.preventDefault();
   var userId = inputId.value, password = inputPassword.value;
 
-  fetchData(`http://${window.location.host}/login`, { userId, password })
-  .catch(error => console.error('Error:', error))
-  .then(response => console.log('Server message:', JSON.stringify(response)));
+  fetchData(`http://${window.location.host}/login`, {userId, password})
+  .then(response => {
+    if(response.status === 403) {
+      M.toast({html: '<span> <i class="small material-icons">announcement</i> 사용자 정보가 존재하지 않습니다.</span>'});
+      inputPassword.value = '';
+      inputPassword.focus();
+    } else if (response.redirected) {
+      window.location.href = response.url;
+    } else {
+      M.toast({html: '<span> <i class="small material-icons">announcement</i> 사바기 요청을 처리할 수 없는 상태입니다.</span>'});
+    }
+  })
+  .catch(error => {
+    M.toast({html: '<span> <i class="small material-icons">announcement</i> 사바기 요청을 처리할 수 없는 상태입니다.</span>'});
+  });
 });
