1a71ab60447a5677655c169147672a6f79e9130c
Ryujeuk <jeuk817@gmail.com>
2019-06-26 16:49:51 +0900

    feat: 로그인창 포커스중 멈추도록 구현
    로그인창의 자식노드들에 포커스가 맞춰질경우 .loginWindowActive 클래스를
    주어 멈추도록 구현함

--- active.js
+++ active.js
@@ -2,3 +2,9 @@ var button = document.getElementById('loginBtn');
 button.addEventListener('click', (event) => {
     alert('Hello world');
 });
+
+const arr = document.querySelectorAll(".loginContainer > input");
+for (var id in arr) {
+    arr[id].addEventListener("focus", (e) => e.target.closest(".loginWindow").classList.toggle("loginWindowActive"))
+    arr[id].addEventListener("blur", (e) => e.target.closest(".loginWindow").classList.toggle("loginWindowActive"))
+}
