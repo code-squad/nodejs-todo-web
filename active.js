21b63a162084f0665a136ece22062205eebe51ac
Ryujeuk <jeuk817@gmail.com>
2019-06-27 06:47:28 +0900

    feat: 로그인창 정지 수정, 스케줄카드 생성 구현
    1. 로그인창에 포커스시 멈추는 로직을 foreach로 수정
    2. text창에 텍스트 입력 후 enter를 누르면 text창이 사라지고 스케줄카드가
    생성되도록 구현

--- active.js
+++ active.js
@@ -4,7 +4,34 @@ button.addEventListener('click', (event) => {
 });
 
 const arr = document.querySelectorAll(".loginContainer > input");
-for (var id in arr) {
-    arr[id].addEventListener("focus", (e) => e.target.closest(".loginWindow").classList.toggle("loginWindowActive"))
-    arr[id].addEventListener("blur", (e) => e.target.closest(".loginWindow").classList.toggle("loginWindowActive"))
-}
+arr.forEach(element => {
+    element.addEventListener("focus", (e) => e.target.closest(".loginWindow").classList.toggle("loginWindowActive"))
+    element.addEventListener("blur", (e) => e.target.closest(".loginWindow").classList.toggle("loginWindowActive"))
+
+});
+
+// var text = document.querySelector(".memoNote");
+// console.log(text)
+// text.addEventListener("keydown", (e) => console.log(e.keyCode));
+document.querySelector(".memoNote").addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+        var text = e.target.value;
+        e.target.insertAdjacentHTML('afterend', `<p class="schedule">${text}</p>`)
+        e.target.parentNode.removeChild(e.target);
+    }
+});
+
+// function makeSchedule() {
+//     var key = window.event.keyCode;
+//     console.log(key)
+
+//     if (key === 13) {
+//         // document.getElementById("aa").innerHTML = document.getElementById("drag1").value;
+//         var textarea = document.getElementById('drag1');
+//         var text = document.getElementById("drag1").value;
+//         textarea.insertAdjacentHTML('afterend', `<p id="schedule">${text}</p>`);
+//     }
+//     else {
+//         return true;
+//     }
+// }
