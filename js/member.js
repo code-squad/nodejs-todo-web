ff8ea5680b526794f97c5b9c641a489afc9adf71
mukeunzi <eunji057@gmail.com>
2019-06-27 14:41:11 +0900

    feat : signUp 메소드 생성
    - 회원 아이디 유효성 검사
    - 회원가입에 성공할 경우 sessionid 생성

--- js/member.js
+++ js/member.js
@@ -1,3 +1,4 @@
+const cookie = require('cookie');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync(`${__dirname}/../data/member.json`);
@@ -22,6 +23,26 @@ const isValidMember = loginData => {
 	return { user_id, user_sid };
 };
 
+const signUp = data => {
+	const { user_id, user_password } = JSON.parse(data);
+
+	const duplicatedId = memberDB
+		.get('members')
+		.find({ user_id })
+		.value();
+
+	if (duplicatedId) {
+		return false;
+	}
+	const user_sid = makeSessionId();
+	memberDB
+		.get('members')
+		.push({ user_id, user_password, user_sid })
+		.write();
+
+	return user_sid;
+};
+
 const makeSessionId = () => {
 	const min = 10000000000000000;
 	const max = 99999999999999999;
@@ -29,4 +50,4 @@ const makeSessionId = () => {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-module.exports = { isValidMember };
+module.exports = { isValidMember, signUp };
