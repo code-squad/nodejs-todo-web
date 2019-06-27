fde5637997af89c3716482328e7db170a407df80
mukeunzi <eunji057@gmail.com>
2019-06-27 16:14:07 +0900

    feat : login 메소드 이름 변경
    - isValidMember => login 으로 변경

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ const load = () => {
 		const signUpButton = document.querySelector('#signUpButton');
 
 		loginButton.addEventListener('click', () => {
-			isValidMember();
+			login();
 		});
 		signUpButton.addEventListener('click', () => {
 			location.href = '/signUp';
@@ -12,7 +12,7 @@ const load = () => {
 	});
 };
 
-const isValidMember = async () => {
+const login = async () => {
 	const user_id = document.querySelector('#id').value;
 	const user_password = document.querySelector('#password').value;
 	const loginData = { user_id, user_password };
@@ -21,7 +21,9 @@ const isValidMember = async () => {
 		const response = await fetch('/login', { method: 'POST', body: JSON.stringify(loginData) });
 		if (response.ok) {
 			const validMember = await response.text();
-			validMember === 'true' ? (location.href = '/') : (location.href = '/login');
+			if (validMember === 'true') {
+				location.href = '/';
+			}
 		} else {
 			location.href = '/error-404';
 		}
