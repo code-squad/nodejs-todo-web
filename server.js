dc1fa339cd97cfa62712c7a46ca21adea194e4e3
ymink16 <yminkdev@gmail.com>
2019-06-27 15:06:47 +0900

    정적 파일 처리
    정적 파일을 public 디렉토리에 정리해두었습니다.
    http 모듈을 이용하여 서버를 생성하고 정적파일들을 불러오는 동작까지
    완료했습니다.

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
 const http = require('http');
+const path = require('path');
 const fs = require('fs');
 
-const parseCookies = (cookie = '') =>
-    cookie
-    .split(';')
-    .map(v => v.split('='))
-    .map(([k, ...vs]) => [k, vs.join('=')])
-    .reduce((acc, [k, v]) => {
-        acc[k.trim()] = decodeURIComponent(v);
-        return acc;
-    }, {});
+const server = http.createServer((req, res) => {
+    const fileType = {
+        '.ico': 'image/x-icon',
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.eot': 'appliaction/vnd.ms-fontobject',
+        '.ttf': 'aplication/font-sfnt'
+    };
+    const ext = path.parse(req.url).ext; // 확장자 정보
+    const publicPath = path.join(__dirname, '/public'); // public 경로
 
-http.createServer((req, res) => {
-    fs.readFile('./index.html', (err, data)=> {
-        if (err) throw err;
-        res.end(data);
-    });
+    if(Object.keys(fileType).includes(ext)) {
+        fs.readFile(`${publicPath}${req.url}`, (err, data) => {
+            if(err) {
+                res.statusCode = 404;
+                res.end('Not found');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', fileType[ext]);
+                res.end(data);
+            }
+        });
+    } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
 
-}).listen(8080, () => {
+        const filePath = path.join(__dirname, 'public/index.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
+    }
+})
+
+server.listen(8080, () => {
     console.log('8080포트에서 대기중');
 });
