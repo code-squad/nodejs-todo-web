4e5aa7c9798d8e2cb64a1653c614edb4786c9c17
mukeunzi <eunji057@gmail.com>
2019-06-27 11:31:12 +0900

    feat : submitSignUpData 메소드 분기
    - isValidSignUpData 유효성 검사
    - submitSignUpData  메소드 에서 signUp/POST 로 회원가입 정보 요청

--- js/sign-up.js
+++ js/sign-up.js
@@ -3,18 +3,38 @@ const load = () => {
 		const signUpButton = document.querySelector('#signUpButton');
 
 		signUpButton.addEventListener('click', () => {
-			inputValidCheck();
+			isValidSignUpData();
 		});
 	});
 };
 
-const inputValidCheck = () => {
+const isValidSignUpData = () => {
 	const id = document.querySelector('#id').value;
 	const password = document.querySelector('#password').value;
 	const rePassword = document.querySelector('#rePassword').value;
 
 	if (isValidId(id) && isValidPassword(password, rePassword)) {
-		submitSignUpData({ id, password });
+		submitSignUpData({ user_id: id, user_password: password });
+	}
+};
+
+const submitSignUpData = async signUpData => {
+	try {
+		const response = await fetch('/signUp', { method: 'POST', body: JSON.stringify(signUpData) });
+		if (response.ok) {
+			const successSignUp = await response.text();
+			if (successSignUp === 'false') {
+				document.querySelector('#password').value = '';
+				document.querySelector('#rePassword').value = '';
+				document.querySelector('#id').focus();
+				alert('이미 사용중인 아이디입니다.');
+			}
+		} else {
+			location.href = '/error-404';
+		}
+	} catch (error) {
+		console.log('error.....', error);
+		location.href = '/error-500';
 	}
 };
 
@@ -41,24 +61,6 @@ const isValidPassword = (password, rePassword) => {
 		return;
 	}
 	return true;
-};
-
-const submitSignUpData = async signUpData => {
-	try {
-		const response = await fetch('/signUp', { method: 'POST', body: JSON.stringify(signUpData) });
-		if (response.ok) {
-			const successSignUp = await response.text();
-			if (successSignUp === 'false') {
-				document.querySelector('#id').focus();
-				alert('이미 사용중인 아이디입니다.');
-			}
-		} else {
-			location.href = '/error-404';
-		}
-	} catch (error) {
-		console.log('error.....', error);
-		location.href = '/error-500';
-	}
 };
 
 load();
