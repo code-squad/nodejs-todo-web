2c77f5162cd32f4b58761ea9300c559f6e6bc89e
MCC <cocahack@naver.com>
2019-06-27 20:43:04 +0900

    feat: 로그인 화면 기능 추가
    회원가입 버튼 추가
    입력값 검증 로직 추가

--- public/login.js
+++ public/login.js
@@ -1,6 +1,19 @@
 var loginBtn = document.getElementById('login-btn');
+var signupBtn = document.getElementById('signup-btn');
 var inputId = document.getElementById('userId');
 var inputPassword = document.getElementById('password');
+
+function makeToast(text = '서버가 요청을 처리할 수 없는 상태입니다.'){
+  M.toast({html: `<span> <i class="small material-icons">announcement</i>${text}</span>`});
+}
+
+function validatePassword(password) {
+  return RegExp(/^[A-Za-z0-9!@#$%^&*-+=-_]{4,16}$/).test(password);
+}
+
+function validateUserId(userId) {
+  return RegExp(/^[a-z][a-z|0-9]{3,11}$/).test(userId);
+}
 
 function fetchData(url = '/todo', data = {}) {
   return fetch(url, {
@@ -15,23 +28,45 @@ function fetchData(url = '/todo', data = {}) {
   .then(response => response);
 }
 
+signupBtn.addEventListener('click', function(e){
+  e.preventDefault();
+  window.location.href = '/signup';
+});
+
 loginBtn.addEventListener('click', function(e){
   e.preventDefault();
   var userId = inputId.value, password = inputPassword.value;
+  var passValidation = true;
+  if(!validateUserId(userId)){
+    makeToast('ID 형식이 맞지 않습니다.')
+    inputId.value = '';
+    inputId.focus();
+    passValidation = false;
+  }
+
+  if(!validatePassword(password)){
+    makeToast('비밀번호 형식이 맞지 않습니다.');
+    inputPassword.value = '';
+    inputPassword.focus();
+    passValidation = false;
+  }
+
+  if(!passValidation) return;
 
   fetchData(`http://${window.location.host}/login`, {userId, password})
   .then(response => {
     if(response.status === 403) {
-      M.toast({html: '<span> <i class="small material-icons">announcement</i> 사용자 정보가 존재하지 않습니다.</span>'});
+      makeToast('일치하는 정보가 없습니다.');
       inputPassword.value = '';
       inputPassword.focus();
     } else if (response.redirected) {
       window.location.href = response.url;
     } else {
-      M.toast({html: '<span> <i class="small material-icons">announcement</i> 사바기 요청을 처리할 수 없는 상태입니다.</span>'});
+      makeToast();
     }
   })
   .catch(error => {
-    M.toast({html: '<span> <i class="small material-icons">announcement</i> 사바기 요청을 처리할 수 없는 상태입니다.</span>'});
+    console.error(error);
+    makeToast();
   });
 });
