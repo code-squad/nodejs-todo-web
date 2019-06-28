c50186ce004bd0e59a16c2f00e479742c06fb860
MCC <cocahack@naver.com>
2019-06-28 16:01:36 +0900

    refactor: init 함수 제거
    init.js 파일을 제거하고 서버 시작 전 디렉토리를 확인하도록 수정

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const server = require('./app');
 const port = process.env.PORT || 8080;
+const fs = require('fs');
+const path = require('path');
+
+(async () => {
+  const dataDir = path.join(process.cwd(), 'data');
+  try {
+    await fs.promises.access((dataDir), fs.F_OK);
+  } catch (error) {
+    if(error.errno === -2) {
+      await fs.promises.mkdir(path.join(dataDir));
+    } else {
+      console.error('Fatal Error: Cannot access Database directory');
+      process.exit(1);
+    }
+  }
+})();
 
 server.listen(port, () => {
   console.log(`Listen in ${port}`);
