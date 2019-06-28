f86cbf162759abe852802d51b3da76e722441011
Ryujeuk <jeuk817@gmail.com>
2019-06-28 17:22:27 +0900

    refactor: 함수정리 1차
    class로 함수를 묶음

--- active.js
+++ active.js
@@ -1,92 +1,115 @@
 
-function holdLoginWindow() {
-    const loginElements = document.querySelectorAll(".loginContainer > input");
-    loginElements.forEach(element => {
-        element.addEventListener("focus", (e) => e.target.closest(".loginWindow").classList.toggle("loginWindowActive"))
-        element.addEventListener("blur", (e) => e.target.closest(".loginWindow").classList.toggle("loginWindowActive"))
+class AddEvent {
+    constructor() {
+        this.card;
+    }
 
-    });
-}
-holdLoginWindow();
+    addSchedule() {
+        const addScheduleButtons = document.querySelectorAll(".addSchedule");
+        addScheduleButtons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                if (e.target.closest(".status").querySelector('.memo').childNodes.length === 0 || e.target.closest(".status").querySelector('.memo').lastChild.className !== 'memoNote') {
+                    const data = '<input type="text" class="memoNote">';
+                    this.insertElement({ target: e.target.closest(".status").querySelector('.memo'), index: 'beforeend', data });
+                    this.setMemoEvent();
+                }
+            })
+        })
+    }
 
-function setMemoEvent() {
-    document.querySelector(".memoNote").addEventListener("keydown", (e) => {
-        if (e.keyCode === 13) {
-            var text = e.target.value;
-            if (text.length === 0) {
-                alert('스케줄을 입력해 주세요.')
-            } else {
-                e.target.insertAdjacentHTML('afterend', `<p class="schedule" draggable="true">${text}</p>`)
-                e.target.parentNode.removeChild(e.target);
+    setMemoEvent() {
+        document.querySelector(".memoNote").addEventListener("keydown", (e) => {
+            if (e.keyCode === 13) {
+                var text = e.target.value;
+                if (text.length === 0) {
+                    alert('스케줄을 입력해 주세요.');
+                } else {
+                    const data = `<p class="schedule" draggable="true">${text}</p>`;
+                    this.insertElement({ target: e.target, index: 'afterend', data });
+                    e.target.parentNode.removeChild(e.target);
+                }
             }
-        }
-    });
+        });
+    }
+
+    holdLoginWindow() {
+        const loginElements = document.querySelectorAll(".loginContainer > input");
+        loginElements.forEach(element => {
+            element.addEventListener("focus", (e) => e.target.closest(".loginWindow").classList.toggle("loginWindowActive"))
+            element.addEventListener("blur", (e) => e.target.closest(".loginWindow").classList.toggle("loginWindowActive"))
+
+        });
+    }
+
+    toggleClass({ target, className }) {
+        target.classList.toggle(`${className}`)
+    }
+
+    insertElement({ target, index, data }) {
+        target.insertAdjacentHTML(`${index}`, data);
+    }
+
+    dragDrop() {
+        document.addEventListener('dragstart', (e) => {
+            e.dataTransfer.setData('Text', e.target.firstChild.nodeValue);
+            this.card = e.target;
+        })
+
+        document.addEventListener("dragenter", (e) => {
+            if (e.target.className === "schedule") {
+                this.toggleClass({ target: e.target, className: 'activeSchedule' });
+            }
+            if (e.target.id == "trashCan") {
+                this.toggleClass({ target: e.target, className: 'activeTrashCan' });
+            }
+
+        }, false);
+
+        document.addEventListener("dragleave", (e) => {
+            if (e.target.className === "schedule activeSchedule") {
+                this.toggleClass({ target: e.target, className: 'activeSchedule' });
+            }
+            if (e.target.id == "trashCan") {
+                this.toggleClass({ target: e.target, className: 'activeTrashCan' });
+            }
+
+        }, false);
+
+        document.addEventListener("dragover", (e) => {
+            e.preventDefault();
+        });
+
+        document.addEventListener("drop", (e) => {
+            e.preventDefault();
+            var data = e.dataTransfer.getData('Text');
+            const schedule = `<p class="schedule" draggable="true">${data}</p>`;
+            if (e.target.className == "schedule activeSchedule") {
+                this.toggleClass({ target: e.target, className: 'activeSchedule' });
+                this.insertElement({ target: e.target, index: 'beforebegin', data: schedule });
+                this.card.parentNode.removeChild(this.card);
+            }
+            if (e.target.className == "addSchedule") {
+                this.insertElement({ target: e.target.closest(".status").querySelector('.memo'), index: 'beforeend', data: schedule });
+                this.card.parentNode.removeChild(this.card);
+            }
+            if (e.target.nodeName === "LI") {
+                this.insertElement({ target: e.target.closest(".status").querySelector('.memo'), index: 'beforeend', data: schedule });
+                this.card.parentNode.removeChild(this.card);
+            }
+            if (e.target.className === "title") {
+                this.insertElement({ target: e.target.closest(".status").querySelector('.memo'), index: 'afterbegin', data: schedule });
+                this.card.parentNode.removeChild(this.card);
+            }
+            if (e.target.id === "trashCan") {
+                this.toggleClass({ target: e.target, className: 'activeTrashCan' });
+                this.card.parentNode.removeChild(this.card);
+            }
+        });
+    }
+
 }
 
-const addScheduleButtons = document.querySelectorAll(".addSchedule");
-addScheduleButtons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        if (e.target.closest(".status").querySelector('.memo').childNodes.length === 0 || e.target.closest(".status").querySelector('.memo').lastChild.className !== 'memoNote') {
-            e.target.closest(".status").querySelector('.memo').insertAdjacentHTML('beforeend', '<input type="text" class="memoNote">');
-
-            setMemoEvent();
-        }
-    })
-})
-
-let card;
-
-document.addEventListener('dragstart', (e) => {
-    e.dataTransfer.setData('Text', e.target.firstChild.nodeValue);
-    card = e.target;
-})
-
-document.addEventListener("dragenter", (e) => {
-    if (e.target.className === "schedule") {
-        e.target.classList.add("activeSchedule");
-    }
-    if (e.target.id == "trashCan") {
-        e.target.classList.add("activeTrashCan");
-    }
-
-}, false);
-
-document.addEventListener("dragleave", (e) => {
-    if (e.target.className === "schedule activeSchedule") {
-        e.target.classList.remove("activeSchedule");
-    }
-    if (e.target.id == "trashCan") {
-        e.target.classList.remove("activeTrashCan");
-    }
-
-}, false);
-
-document.addEventListener("dragover", (e) => {
-    e.preventDefault();
-});
-
-document.addEventListener("drop", (e) => {
-    e.preventDefault();
-    var data = e.dataTransfer.getData('Text');
-    if (e.target.className == "schedule activeSchedule") {
-        e.target.classList.remove("activeSchedule");
-        e.target.insertAdjacentHTML('beforebegin', `<p class="schedule" draggable="true">${data}</p>`);
-        card.parentNode.removeChild(card);
-    }
-    if (e.target.className == "addSchedule") {
-        e.target.closest(".status").querySelector('.memo').insertAdjacentHTML('beforeend', `<p class="schedule" draggable="true">${data}</p>`);
-        card.parentNode.removeChild(card);
-    }
-    if (e.target.nodeName === "LI") {
-        e.target.closest(".status").querySelector('.memo').insertAdjacentHTML('beforeend', `<p class="schedule" draggable="true">${data}</p>`);
-        card.parentNode.removeChild(card);
-    }
-    if (e.target.className === "title") {
-        e.target.closest(".status").querySelector('.memo').insertAdjacentHTML('afterbegin', `<p class="schedule" draggable="true">${data}</p>`);
-        card.parentNode.removeChild(card);
-    }
-    if (e.target.id === "trashCan") {
-        e.target.classList.remove("activeTrashCan");
-        card.parentNode.removeChild(card);
-    }
-});
+const addEvent = new AddEvent();
+addEvent.dragDrop();
+addEvent.holdLoginWindow();
+addEvent.addSchedule();
