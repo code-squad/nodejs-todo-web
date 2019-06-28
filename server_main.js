dac6ef4fcc429c2facadc9660dec80782fb4f50b
Min-92 <0201.mj.lee@gmail.com>
2019-06-28 20:34:20 +0900

    feat : 로그인, 세션, 쿠키 구현

--- server_main.js
+++ server_main.js
@@ -1,13 +1,49 @@
-var http = require('http');
-var fs = require('fs');
-var app = http.createServer((request, response) => {
+const http = require('http');
+const fs = require('fs');
+const cookie = require('cookie');
+const UserManager = require('./user_manager.js');
+const Session = require('./session.js');
+const userManager = new UserManager(fs);
+const session = new Session();
+const app = http.createServer(async (request, response) => {
+    let url = request.url;
+    console.log(`${request.method}, url = ${url}`);
     if (request.method === 'GET') {
-        let url = request.url;
-        if (request.url === "/login" || request.url === '/') {
-        // if (request.url === '/') {
+        if (request.url === "/login" || request.url === '/' || request.url === "/login.html") {
+            if (request.headers.cookie) {
+                const cookies = cookie.parse(request.headers.cookie);
+                if (session.isValidSessionID(cookies.sessionID)) {
+                    response.writeHead(302, {
+                        'Location': './index',
+                        'Content-Type': 'text/html',
+                        'Set-Cookie': [`sessionID = ${cookies.sessionID}; Max-Age = ${60 * 60 / 2}`]
+                    });
+                    return response.end('redirection');
+                }
+            }
             url = '/login.html';
             response.writeHead(200);
-            response.end(fs.readFileSync(__dirname + url));
+            return response.end(await userManager.myReadFile(__dirname + url));
+        }
+        if (request.url === "/index" || request.url === "/index.html") {
+            if (request.headers.cookie) {
+                const cookies = cookie.parse(request.headers.cookie);
+                if (session.isValidSessionID(cookies.sessionID)) {
+                    response.writeHead(200, {
+                        'Content-Type': 'text/html',
+                        'Set-Cookie': [`sessionID = ${
+                            cookies.sessionID}; Max-Age = ${60 * 60 / 2}`]
+                    });
+                    url = '/index.html';
+                    return response.end(await userManager.myReadFile(__dirname + url));
+                }
+            }
+            response.writeHead(302, {
+                'Location': './login',
+                'Content-Type': 'text/html',
+                'Set-Cookie': [`sessionID = ''; Max-Age = 0`]
+            });
+            return response.end('redirection');
         }
         return fs.readFile(`.${request.url}`, (err, data) => {
             if (err) {
@@ -16,34 +52,40 @@ var app = http.createServer((request, response) => {
             }
             return response.end(data);
         });
+
+
+    } else if (request.method === 'POST') {
+        if (request.url === "/login" || request.url === '/') {
+            let body = '';
+            request.on('data', (data) => {
+                body += data;
+            });
+            if (request.headers.cookie) {
+                const cookies = cookie.parse(request.headers.cookie);
+            }
+            return request.on('end', async () => {
+                const id = JSON.parse(body).id;
+                const pw = Number(JSON.parse(body).pw);
+                const userInfo = await userManager.logIn(id, pw);
+                if (!userInfo) {
+                    response.writeHead(409, 'CONFLICT');
+                    return response.end('CONFLICT');
+                } else {
+                    const sessionID = session.getSessionID(userInfo);
+                    response.writeHead(302, {
+                        'Content-Type': 'text/html',
+                        'Location': './index',
+                        'Set-Cookie': [`sessionID = ${sessionID}; Max-Age = ${60 * 60 / 2}`]
+                    });
+                    console.log(`user "${id}" has logged in. sessionID = "${sessionID}"`);
+                    return response.end('set cookie');
+                }
+            });
+        }
     }
-    // console.log(request);
 
     response.writeHead(404, 'NOT FOUND');
     return response.end('NOT FOUND');
-
-    // var url = request.url;
-    // if(request.url === '/'){
-    //   url = '/login.html';
-    // }
-    // // if(request.url == '/favicon.ico'){
-    // //     response.writeHead(404);
-    // //     response.end();
-    // //     return response.writeHead(404);
-    // // }
-    // response.writeHead(200); // 200 : ok
-    // response.end(fs.readFileSync(__dirname + url));
-    // var url = request.url;
-    // if(request.url == '/'){
-    //   url = '/login.html';
-    // }
-    // if(request.url == '/favicon.ico'){
-    //     response.writeHead(404);
-    //     response.end();
-    //     return response.writeHead(404);
-    // }
-    // response.writeHead(200); // 200 : ok
-    // response.end(fs.readFileSync(__dirname + url));
 
 });
 app.listen(3000);
