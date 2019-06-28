3d8ac9236440143fa5d9ccc108e8f5b0ada500d5
Jimin Yeon <gmini.y@gmail.com>
2019-06-28 11:33:32 +0900

    [feat] session check logic

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require('http');
 const {parseCookie} = require('./util');
 const port = 8080;
+const session = {};
+
 
 const server = http.createServer((request, response) => {
-    if(request.url === '/') console.log(parseCookie(request.headers.cookie));
+    const cookie = parseCookie(request.headers.cookie);
+    if(!(cookie.session in session)) {
+        if(request.url === '/') {
+            response.end(cookie.session);
+        }
+    }
 });
 
 server.listen(port, () => {
