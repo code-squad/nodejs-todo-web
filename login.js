a697999b67d7f0921a13a2a7c8344ad41e01147e
Min-92 <0201.mj.lee@gmail.com>
2019-06-28 20:35:59 +0900

    feat : 로그인 이벤트 구현

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
-class Login{
-    constructor(){  
+class Login {
+    constructor() {
         this.main;
         this.loginBox;
         this.inputBox;
@@ -10,38 +10,39 @@ class Login{
         this.signUpButton;
     }
 
-    setLoginPage(){
+    setLoginPage() {
         this.main = document.querySelector('main');
         this.main.appendChild(this.setLoginBox());
     }
 
-    setLoginBox(){
-        this.loginBox = document.createElement('div');
+    setLoginBox() {
+        this.loginBox = document.createElement('form');
         this.loginBox.appendChild(this.setInputBox());
         this.loginBox.appendChild(this.setButtonBox());
         return this.loginBox;
     }
 
-    setInputBox(){
+    setInputBox() {
         this.inputBox = document.createElement('div');
         this.inputBox.appendChild(this.setIdTextarea());
         this.inputBox.appendChild(this.setPwTextarea());
-        
+
         return this.inputBox;
     }
-    
-    setIdTextarea(){
-        this.idTextarea = document.createElement('textarea');
+
+    setIdTextarea() {
+        this.idTextarea = document.createElement('input');
         this.idTextarea.placeholder = 'please input your ID';
         return this.idTextarea;
     }
-    setPwTextarea(){
-        this.pwTextarea = document.createElement('textarea');
+    setPwTextarea() {
+        // name을 만들
+        this.pwTextarea = document.createElement('input');
         this.pwTextarea.placeholder = 'please input your PW';
         return this.pwTextarea;
     }
 
-    setButtonBox(){
+    setButtonBox() {
         this.buttonBox = document.createElement('div');
         this.buttonBox.appendChild(this.setLoginButton());
         this.buttonBox.appendChild(this.setSignUpButton());
@@ -49,32 +50,57 @@ class Login{
         return this.buttonBox;
     }
 
-    setLoginButton(){
+    setLoginButton() {
         this.loginButton = document.createElement('button');
-        this.loginButton.innerText = 'login';
+        this.loginButton.innerText = 'Log in';
         this.addLoginButtonListener(this.loginButton);
         return this.loginButton;
     }
-    setSignUpButton(){
+    setSignUpButton() {
         this.signUpButton = document.createElement('button');
-        this.signUpButton.innerText = 'sign up';
+        this.signUpButton.innerText = 'Sign up';
         this.addSignUpButtonListener(this.signUpButton);
-        
-        return this.signUpButton;
-    }   
 
-    addLoginButtonListener(button){
+        return this.signUpButton;
+    }
+
+    addLoginButtonListener(button) {
         button.addEventListener('click', event => {
-            if(this.idTextarea.value === '' || this.pwTextarea === ''){
+            event.preventDefault();
+            if (this.idTextarea.value === '' || this.pwTextarea === '') {
                 alert('id나 pw가 올바르지 않습니다.');
                 return;
             }
+            const id = this.idTextarea.value;
+            const pw = this.pwTextarea.value;
+            console.log(id);
+            console.log(pw);
+
+            const request = new XMLHttpRequest();
+            request.onload = () => {
+                const url = 'http://localhost:3000/index';
+                if (request.status === 409) {
+                    return alert("id나 pw가 올바르지 않습니다.");
+                } else if (request.status === 200) {
+                    if (request.responseURL === url) {
+                        return window.location.replace(request.responseURL);
+                    }
+                }
+            }
+
+            request.open('POST', './login');
+            request.setRequestHeader('Content-type', 'application/json');
+            request.send(JSON.stringify({ id: id, pw: pw }));
+            this.idTextarea.value = '';
+            this.pwTextarea.value = '';
+
+
         });
     }
-    
-    addSignUpButtonListener(button){
+
+    addSignUpButtonListener(button) {
         button.addEventListener('click', event => {
-            if(this.idTextarea.value === '' || this.pwTextarea === ''){
+            if (this.idTextarea.value === '' || this.pwTextarea === '') {
                 alert('id나 pw가 올바르지 않습니다.');
                 return;
             }
