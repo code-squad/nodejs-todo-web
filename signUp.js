da33f1248226a627a3c8534e4b684d139fac5933
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-29 21:15:50 +0900

    [refactor] 회원 가입 기능 수정
    - signUp.html form 태그 형식 삭제
    - Javascript API fetch 함수 사용
    - fetch 함수를 통해 POST 처리

---
+++ signUp.js
@@ -0,0 +1,25 @@
+document.querySelector('#signUpButton').addEventListener('click', () => {
+    const id = document.querySelector('#userID').value;
+    const pw = document.querySelector('#userPW').value;
+    fetch('http://localhost:8888/signUpCheck', {
+        method: 'post',
+        // redirect: 'follow',
+        body: JSON.stringify({ id: id, pw: pw }),
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+    }).then((response) => {
+        switch (response.status) {
+            case 200:
+                alert(`존재 하는 회원 정보 입니다!`);
+                break;
+            case 301:
+                alert(`회원 가입 완료!`);
+                window.location.href = '/';
+                break;
+            default:
+                alert(`HTTP status : ${response.status}`);
+                break;
+        }
+    });
+});
+
+// 중복 체크 하는 부분 추가하기..
