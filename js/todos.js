ff8078cc32cf7ea269dac098ff74a644381564ff
mukeunzi <eunji057@gmail.com>
2019-06-29 19:08:49 +0900

    feat : makeTodosId 메소드 생성
    - 4 자리 랜덤값으로 todo 의 고유 id 를 생성하는 메소드

--- js/todos.js
+++ js/todos.js
@@ -5,4 +5,11 @@ const getTodosList = user_id => {
 	return todosList;
 };
 
+const makeTodosId = () => {
+	const min = 1000;
+	const max = 9999;
+
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 module.exports = { getTodosList };
