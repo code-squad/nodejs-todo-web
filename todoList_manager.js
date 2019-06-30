abdc2dec2107d4278ab670e3ca83dcc9171a14cf
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-30 15:10:09 +0900

    [feat] .csv 파일에서 데이터를 읽어오는  함수 추가

--- todoList_manager.js
+++ todoList_manager.js
@@ -3,12 +3,23 @@ const fs = require('fs');
 module.exports.initTodoList = async (id) => {
     console.time(`[ TodoList Mananger ] Init todoList `);
     const template = { 'id': id, 'contents' : { 'todo' : [], 'doing': [], 'done' : [] } };
-    const writeData = JSON.stringify(template) + ',';
+    const writeData = JSON.stringify(template) + ',햣';
     const option = { encoding: 'utf-8', flag: 'a' };
     fs.appendFile('./todoList_Information.csv', writeData, option, (error) => {
         if (error) throw error;
         console.log("todoList is appended to file successfully.");
     });
     console.timeEnd(`[ TodoList Mananger ] Init todoList `);
+}
+
+module.exports.readTodoList = () => {
+    return new Promise((resolve) => {
+        console.time(`[ TodoList Mananger ] Read TodoList `);
+        fs.readFile('./todoList_Information.csv', 'utf-8', (error, data) => {
+            if (error) throw error;
+            resolve(`[${data.substr(0, data.length)}]`);
+        });
+        console.timeEnd(`[ TodoList Mananger ] Read TodoList `);
+    });
 }
 
