d2d759c4d4b26d9d2a9752552408e9a2991b92ef
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-30 17:55:44 +0900

    Test : getPage() 메서드 테스트 코드 작성
    mock http로 요청해 status와 content-type 검증

--- test/controller/register.spec.js
+++ test/controller/register.spec.js
@@ -1,2 +1,32 @@
+const should = require('should');
+const sinon = require('sinon');
+const httpMocks = require('node-mocks-http');
+
 const registerController = require('../../controller/register');
 
+describe('register Controller Test', () => {
+  let response;
+  let request;
+
+  beforeEach(() => {
+    response = httpMocks.createResponse();
+    request = httpMocks.createRequest();
+  })
+
+  afterEach(() => {
+    request = null;
+    response = null;
+  })
+
+  describe('getPage()', () => {
+    it('getPage()를 요청하면 status 200과 content-type으로 text/html을 내려준다', async () => {
+      const next = 0;
+      await registerController.getPage()(request, response, next);
+      const contentType = await response.getHeader('Content-Type');
+      const statusCode = await response.statusCode;
+
+      should(contentType).equal('text/html');
+      should(statusCode).equal(200);
+    })
+  })
+})
