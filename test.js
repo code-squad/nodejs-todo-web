730655c44acccd3fd063d85ffb249db34573c76d
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-30 15:13:00 +0900

    [test] readTodoList 함수 테스트 코드 작성

--- test.js
+++ test.js
@@ -1,76 +1,17 @@
+const todoListManager = require('./todoList_manager');
 const request   = require('supertest');
 const server    = require('./server');
 
-describe('Test the root path', () => {
-    // test('POST 메소드 테스트', (done) => {
-    //     request(server)
-    //     .post('/SignIn.html')
-    //     .then((response) => {
-    //         expect(response.statusCode).toEqual(200);
-    //         done();
-    //     });
-    // });
-    test('POST 회원가입 테스트', (done) => {
-        request(server)
-        .post('/signUpCheck')
-        .send('id=hyodol1225&pw=1234')
-        .then((response) => {
-            expect(response.statusCode).toEqual(200);
-            done();
-        });
-    });
-    test('POST 로그인 테스트', (done) => {
-        request(server)
-        .post('/signInCheck')
-        .send('id=test1&pw=1234')
-        .then((response) => {
-            expect(response.statusCode).toEqual(200);
-            done();
-        });
-    });
-    test('POST 로그인 테스트', (done) => {
-        request(server)
-        .post('/signInCheck')
-        .send('id=test&pw=1234')
-        .then((response) => {
-            expect(response.statusCode).toEqual(200);
-            done();
-        });
-    });
-    test('POST 로그인 테스트', (done) => {
-        request(server)
-        .post('/signInCheck')
-        .send('id=test1123&pw=1234')
-        .then((response) => {
-            expect(response.statusCode).toEqual(200);
-            done();
-        });
-    });
-    test('POST 로그인 테스트', (done) => {
-        request(server)
-        .post('/signInCheck')
-        .send('id=h1&pw=1234')
-        .then((response) => {
-            expect(response.statusCode).toEqual(200);
-            done();
-        });
-    });
-    test('POST 로그인 테스트', (done) => {
-        request(server)
-        .post('/signInCheck')
-        .send('id=test2&pw=1234')
-        .then((response) => {
-            expect(response.statusCode).toEqual(200);
-            done();
-        });
-    });
-    test('POST 로그인 테스트', (done) => {
-        request(server)
-        .post('/signInCheck')
-        .send('id=hyo&pw=1234')
-        .then((response) => {
-            expect(response.statusCode).toEqual(200);
-            done();
+describe('TodoList Manager Test', () => {
+    test('todoList parse', async () => {
+        const todoList = JSON.parse(await todoListManager.readTodoList());
+        console.log(todoList);
+        expect(Array.isArray(todoList)).toEqual(true);
+        todoList.forEach((element) => {
+            if (element.id === 'hyodol') {
+                console.log(element.contents);
+                expect(Array.isArray(element.contents.todo)).toEqual(true);
+            }
         });
     });
 });
