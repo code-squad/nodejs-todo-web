c9897381ff2ba94a4783d1f8fc9e343bc4f45ac6
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-30 18:40:10 +0900

    Test : 아이디 중복 체크 메서드 테스트 작성
    동일한 아이디일 경우 deny 반환, 아닐 경우 success 반환

--- test/controller/register.spec.js
+++ test/controller/register.spec.js
@@ -1,7 +1,6 @@
 const should = require('should');
 const sinon = require('sinon');
 const httpMocks = require('node-mocks-http');
-
 const registerController = require('../../controller/register');
 
 describe('register Controller Test', () => {
@@ -20,7 +19,7 @@ describe('register Controller Test', () => {
 
   describe('getPage()', () => {
     it('getPage()를 요청하면 status 200과 content-type으로 text/html을 내려준다', async () => {
-      const next = 0;
+      const next = () => {};
       await registerController.getPage()(request, response, next);
       const contentType = await response.getHeader('Content-Type');
       const statusCode = await response.statusCode;
@@ -29,4 +28,38 @@ describe('register Controller Test', () => {
       should(statusCode).equal(200);
     })
   })
+
+  describe('checkDupleID()', () => {
+    it('동일한 아이디가 있을 경우, 응답으로 deny를 반환한다', async() => {
+      const next = () => {};
+      request.url = '/register/korea'
+
+      await registerController.checkDupleId()(request, response, next);
+
+      const contentType = await response.getHeader('Content-Type');
+      const statusCode = await response.statusCode;
+      const answer = await response._getData();
+      
+      should(contentType).equal('text/plain');
+      should(statusCode).equal(200);
+      should(answer).equal('deny');
+    })
+
+    it('동일한 아이디가 아닐 경우, 응답으로 success 반환', async() => {
+      const next = () => {};
+      request.url = '/register/uniqueID'
+
+      await registerController.checkDupleId()(request, response, next);
+
+      const contentType = await response.getHeader('Content-Type');
+      const statusCode = await response.statusCode;
+      const answer = await response._getData();
+      
+      should(contentType).equal('text/plain');
+      should(statusCode).equal(200);
+      should(answer).equal('success');
+    })
+  })
+
+
 })
