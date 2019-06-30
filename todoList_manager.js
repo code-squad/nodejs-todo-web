fc6df5d038b2c0c5617497e915605a3acbf5ee7c
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-30 14:39:52 +0900

    [test] initTodoList 함수 테스트

--- todoList_manager.js
+++ todoList_manager.js
@@ -3,7 +3,12 @@ const fs = require('fs');
 module.exports.initTodoList = async (id) => {
     console.time(`[ TodoList Mananger ] Init todoList `);
     const template = { 'id': id, 'contents' : { 'todo' : [], 'doing': [], 'done' : [] } };
-    const writeData = JSON.stringify(template) + '$';
+    const writeData = JSON.stringify(template) + ',';
+    const option = { encoding: 'utf-8', flag: 'a' };
+    fs.appendFile('./todoList_Information.csv', writeData, option, (error) => {
+        if (error) throw error;
+        console.log("todoList is appended to file successfully.");
+    });
     console.timeEnd(`[ TodoList Mananger ] Init todoList `);
 }
 
