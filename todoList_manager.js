9e886d5ba7e5bb0e7e1b35e63a0bb346b3f9b19f
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-30 16:12:47 +0900

    [refactor] initTodoList 함수의 내부 로직 변경

--- todoList_manager.js
+++ todoList_manager.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 
 module.exports.initTodoList = async (id) => {
     console.time(`[ TodoList Mananger ] Init todoList `);
-    const template = { 'id': id, 'contents' : { 'todo' : [], 'doing': [], 'done' : [] } };
-    const writeData = JSON.stringify(template) + ',햣';
+    const writeData = `"${id}":{"todo":[],"doing":[],"done":[]},`;
     const option = { encoding: 'utf-8', flag: 'a' };
     fs.appendFile('./todoList_Information.csv', writeData, option, (error) => {
         if (error) throw error;
@@ -17,7 +16,7 @@ module.exports.readTodoList = () => {
         console.time(`[ TodoList Mananger ] Read TodoList `);
         fs.readFile('./todoList_Information.csv', 'utf-8', (error, data) => {
             if (error) throw error;
-            resolve(`[${data.substr(0, data.length)}]`);
+            resolve(`{${data.substr(0, data.length - 1)}}`);
         });
         console.timeEnd(`[ TodoList Mananger ] Read TodoList `);
     });
