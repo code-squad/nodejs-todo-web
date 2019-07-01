68b7e41d48479bc84030740e36d00a7ea78d3bcb
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-01 17:57:35 +0900

    [refactor] 불 필요한 코드 제거

--- signIn.js
+++ signIn.js
@@ -1,21 +1,13 @@
 document.querySelector('#signInButton').addEventListener('click', () => {
     const id = document.querySelector('#userID').value;
     const pw = document.querySelector('#userPW').value;
-    fetch('http://localhost:8888/signInCheck', {
-        method: 'post',
-        body: JSON.stringify({ id: id, pw: pw }),
-        headers: new Headers({ 'Content-Type': 'application/json' }),
-    }).then((response) => { 
+    const body = JSON.stringify({ id : id, pw : pw });
+    fetch('http://localhost:8888/signInCheck', { method : 'post', body : body })
+    .then(response => { 
         switch (response.status) {
-            case 200:
-                alert(`회원 정보가 존재하지 않습니다!`);
-                break;
-            case 301:
-                window.location = '/todoList';
-                break;
-            default:
-                alert(`HTTP status : ${response.status}`);
-                break;
+            case 200: alert(`회원 정보가 존재하지 않습니다!`); break;
+            case 301: window.location = '/todoList'; break;
+            default: alert(`HTTP status : ${response.status}`); break;
         }
     });
 });
--- signUp.js
+++ signUp.js
@@ -1,24 +1,13 @@
 document.querySelector('#signUpButton').addEventListener('click', () => {
     const id = document.querySelector('#userID').value;
     const pw = document.querySelector('#userPW').value;
-    fetch('http://localhost:8888/signUpCheck', {
-        method: 'post',
-        body: JSON.stringify({ id: id, pw: pw }),
-        headers: new Headers({ 'Content-Type': 'application/json' }),
-    }).then((response) => {
+    const body = JSON.stringify({ id : id, pw : pw });
+    fetch('http://localhost:8888/signUpCheck', { method : 'post', body : body })
+    .then(response => {
         switch (response.status) {
-            case 200:
-                alert(`존재 하는 회원 정보 입니다!`);
-                break;
-            case 301:
-                alert(`회원 가입 완료!`);
-                window.location = '/';
-                break;
-            default:
-                alert(`HTTP status : ${response.status}`);
-                break;
+            case 200: alert(`존재 하는 회원 정보 입니다!`); break;
+            case 301: alert(`회원 가입 완료!`); window.location = '/'; break;
+            default: alert(`HTTP status : ${response.status}`); break;
         }
     });
 });
-
-// 중복 체크 하는 부분 추가하기..
