c217194aa03a5e1a52c0d77b5c2729386ad2d979
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-01 18:02:38 +0900

    [test] TodoList에 Item이 추가 되는지 테스트

--- test.js
+++ test.js
@@ -4,11 +4,21 @@ const server    = require('./server');
 
 describe('TodoList Manager Test', () => {
     test('todoList parse', async () => {
-        const todoList = JSON.parse(await todoListManager.readTodoList());
-        console.log(todoList);
-        console.log(todoList['hyodol92']);
-        expect(Array.isArray(todoList['hyodol92'].todo)).toEqual(true);
-        expect(Array.isArray(todoList['hyodol92'].doing)).toEqual(true);
-        expect(Array.isArray(todoList['hyodol92'].todo)).toEqual(true);
+        const jsonData = JSON.parse(await todoListManager.readTodoList());
+        console.log(jsonData);
+        console.log(jsonData['hyodol92']);
+        const data = { id : 'hyodol92', contents : jsonData['hyodol92'] };
+        console.log(data.contents);
+        expect(Array.isArray(jsonData['hyodol92'].todo)).toEqual(true);
+        expect(Array.isArray(jsonData['hyodol92'].doing)).toEqual(true);
+        expect(Array.isArray(jsonData['hyodol92'].todo)).toEqual(true);
+    });
+    test('add Test', async () => {
+        const data = {type: 'todo', index: '2', value: '네트워크 공부하기'};
+        const jsonData = JSON.parse(await todoListManager.readTodoList());
+        const todoList = jsonData['hyodol92'];
+        console.log(todoList[data.type]);
+        todoList[data.type].splice(parseInt(data.index), 0, data.value);
+        console.log(todoList[data.type]);
     });
 });
