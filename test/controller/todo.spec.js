187f9acf25617ce794419226c2e523e86776bc81
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-01 20:02:56 +0900

    Feat : 업데이트, 삭제 요청 메서드 테스트 코드 작성

--- test/controller/todo.spec.js
+++ test/controller/todo.spec.js
@@ -2,6 +2,7 @@ const todoController = require('../../controller/todo');
 const should = require('should');
 const httpMocks = require('node-mocks-http');
 const cryptoUtil = require('../../utils/crypto-util');
+const csvParser = require('../../utils/csv-parser');
 
 describe('todoController Test', () => {
   let response;
@@ -52,21 +53,64 @@ describe('todoController Test', () => {
     })
   })
 
-  describe('addTodo()', () => {
+  describe('추가, 업데이트, 삭제 시나리오', () => {
+    let ajaxAnswer;
     it('요청 객체로 데이터 추가를 요청했을 때, text/plain 형태의 숫자로 string 반환', async () => {
+      
+      // 데이터 추가
       request.body = {'data' : 'mockData', 'type' : "todo"};
       request.session = {'userID' : 'uniqueID'};
 
       await todoController.addTodo()(request, response, next);
 
-      const statusCode = await response.statusCode;
-      const contentType = await response.getHeader('Content-Type');
-      const ajaxAnswer = await response._getData();
+      let statusCode = await response.statusCode;
+      let contentType = await response.getHeader('Content-Type');
+      ajaxAnswer = await response._getData();
 
       should(contentType).equal('text/plain');
       should(statusCode).equal(200);
       should(typeof (ajaxAnswer*1)).equal('number');
     })
-  })
 
+    it('이전에 추가한 데이터의 업데이트 요청 시 업데이트 데이터가 db 반영 후 success 문자열 반환', async () => {
+      
+      // 데이터 업데이트
+      const updateType = 'doing';
+      request.url = `/todos/${ajaxAnswer}`;
+      request.body = {'type' : updateType};
+
+      await todoController.updateTodo()(request, response, next);
+
+      statusCode = await response.statusCode;
+      contentType = await response.getHeader('Content-Type');
+      
+      should(contentType).equal('text/plain');
+      should(statusCode).equal(200);
+      
+      const allDataObj = await csvParser.getKeyValueObj('./db/todoList.csv');
+      const card = allDataObj[ajaxAnswer];
+      should(card.type).equal(updateType);
+      
+      const ajaxStr = await response._getData();
+      should(ajaxStr).equal('success');
+    })
+
+    it('데이터 삭제 요청 시 해당 아이템 삭제', async () => {
+      request.url = `/todos/${ajaxAnswer}`;
+      await todoController.deleteTodo()(request, response, next);
+
+      statusCode = await response.statusCode;
+      contentType = await response.getHeader('Content-Type');
+      
+      should(contentType).equal('text/plain');
+      should(statusCode).equal(200);
+      
+      const allDataObj = await csvParser.getKeyValueObj('./db/todoList.csv');
+      const card = allDataObj[ajaxAnswer];
+      should(card).equal(undefined);
+      
+      ajaxAnswer = await response._getData();
+      should(ajaxAnswer).equal('success');
+    })
+  })
 })
