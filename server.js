349a3b03348a0eb8e0fbec79c2c516290416c8de
Jimin Yeon <gmini.y@gmail.com>
2019-07-02 16:45:58 +0900

    [refactor] if문을 switch 로 변경

--- server.js
+++ server.js
@@ -25,119 +25,122 @@ const server = http.createServer(async (request, response) => {
         if (cookie.session in session) {
             const sid = cookie.session;
             const user = session[sid];
-            if (method === 'GET') {
-                if (request.url === '/') {
-                    const mainHTML = await model.readStaticFile('./main.html');
-                    response.statusCode = 200;
-                    response.end(mainHTML);
-                }
-                if (request.url === '/items') {
-                    const items = JSON.parse(await model.readStaticFile('./items.json'))[user];
-                    response.writeHead(200, {'Content-Type': 'application/json'});
-                    response.end(JSON.stringify(items));
-                }
-            } else if (method === 'POST') {
-                if (request.url === '/items') {       
-                    let body = [];
-                    request.on('data', (chunk) => {
-                        body.push(chunk);
-                    }).on('end', async () => {
-                        body = Buffer.concat(body).toString();
-                        const item = JSON.parse(body);
-                        const id = await addController.add(user, item);
-                        if(id) {
-                            response.statusCode = 200;
-                            response.end(`${id}`);
-                        } else {
-                            response.statusCode = 400;
-                            response.end();
-                        }
-                    });
-                }
-            } else if (method === 'PATCH') {
-                if (request.url === '/items') {       
-                    let body = [];
-                    request.on('data', (chunk) => {
-                        body.push(chunk);
-                    }).on('end', async () => {
-                        body = Buffer.concat(body).toString();
-                        const { targetID, siblingID, status } = JSON.parse(body);
-                        if (await updateController.update({ user, targetID, siblingID, status })) {
-                            response.statusCode = 200;
-                            response.end();
-                        } else {
-                            response.statusCode = 400;
-                            response.end();
-                        }
-                    });
-                }
-            } else if (method === 'DELETE') {
-                if (request.url === '/auth') {
-                    delete session[sid];
-                    response.writeHead(200, {'Set-Cookie': [`session = ''; Max-Age = 0`]});
-                    response.end();
-                }
-
-                if (request.url === '/items') {       
-                    let body = [];
-                    request.on('data', (chunk) => {
-                        body.push(chunk);
-                    }).on('end', async () => {
-                        const id = Buffer.concat(body).toString();
-                        if(deleteController.delete(user, id)) {
-                            response.statusCode = 200;
-                            response.end();
-                        }
-                        response.statusCode = 400;
+            switch(method) {
+                case 'GET':
+                    if (request.url === '/') {
+                        const mainHTML = await model.readStaticFile('./main.html');
+                        response.statusCode = 200;
+                        response.end(mainHTML);
+                    } else if (request.url === '/items') {
+                        const items = JSON.parse(await model.readStaticFile('./items.json'))[user];
+                        response.writeHead(200, {'Content-Type': 'application/json'});
+                        response.end(JSON.stringify(items));
+                    }
+                    break;
+                case 'POST':
+                    if (request.url === '/items') {       
+                        let body = [];
+                        request.on('data', (chunk) => {
+                            body.push(chunk);
+                        }).on('end', async () => {
+                            body = Buffer.concat(body).toString();
+                            const item = JSON.parse(body);
+                            const id = await addController.add(user, item);
+                            if(id) {
+                                response.statusCode = 200;
+                                response.end(`${id}`);
+                            } else {
+                                response.statusCode = 503;
+                                response.end();
+                            }
+                        });
+                    }
+                    break;
+                case 'PATCH':
+                    if (request.url === '/items') {       
+                        let body = [];
+                        request.on('data', (chunk) => {
+                            body.push(chunk);
+                        }).on('end', async () => {
+                            body = Buffer.concat(body).toString();
+                            const { targetID, siblingID, status } = JSON.parse(body);
+                            if (await updateController.update({ user, targetID, siblingID, status })) {
+                                response.statusCode = 200;
+                                response.end();
+                            } else {
+                                response.statusCode = 503;
+                                response.end();
+                            }
+                        });
+                    }
+                    break;
+                case 'DELETE':
+                    if (request.url === '/auth') {
+                        delete session[sid];
+                        response.writeHead(200, {'Set-Cookie': [`session = ''; Max-Age = 0`]});
                         response.end();
-                    });
-                }
+                    } else if (request.url === '/items') {       
+                        let body = [];
+                        request.on('data', (chunk) => {
+                            body.push(chunk);
+                        }).on('end', async () => {
+                            const id = Buffer.concat(body).toString();
+                            if(deleteController.delete(user, id)) {
+                                response.statusCode = 200;
+                                response.end();
+                            }
+                            response.statusCode = 503;
+                            response.end();
+                        });
+                    }
+                    break;
             }
         }
     }
-    if (method === 'GET') {
-        if (request.url === '/') {
-            const loginHTML = await model.readStaticFile('./login.html');
-            response.statusCode = 200;
-            response.end(loginHTML);
-        }
 
-    } else if (method === 'POST') {
-        if (request.url === '/auth') {
-            let body = [];
-            request.on('data', (chunk) => {
-                body.push(chunk);
-            }).on('end', async () => {
-                body = Buffer.concat(body).toString();
-                const {id, password} = JSON.parse(body);
-                if (await loginController.login(id, password)) {
-                    const sid = generateRandomInt();
-                    session[sid] = id;
-                    response.writeHead(200, {'Set-Cookie': `session =${sid}; HttpOnly`});
-                    response.end("success");
-                } else {
-                    response.statusCode = 200;
-                    response.end("fail");
-                }
-            });
-        }
-
-        if (request.url === '/members') {
-            let body = [];
-            request.on('data', (chunk) => {
-                body.push(chunk);
-            }).on('end', async () => {
-                body = Buffer.concat(body).toString();
-                const { id, password } = JSON.parse(body);
-                if (await signupController.signup(id, password)) {
-                    response.statusCode = 200;
-                    response.end("success");
-                } else {
-                    response.statusCode = 200;
-                    response.end("fail");
-                }
-            });
-        }
+    switch (method) {
+        case 'GET':
+            if (request.url === '/') {
+                const loginHTML = await model.readStaticFile('./login.html');
+                response.statusCode = 200;
+                response.end(loginHTML);
+            }
+            break;
+        case 'POST':
+            if (request.url === '/auth') {
+                let body = [];
+                request.on('data', (chunk) => {
+                    body.push(chunk);
+                }).on('end', async () => {
+                    body = Buffer.concat(body).toString();
+                    const {id, password} = JSON.parse(body);
+                    if (await loginController.login(id, password)) {
+                        const sid = generateRandomInt();
+                        session[sid] = id;
+                        response.writeHead(200, {'Set-Cookie': `session =${sid}; HttpOnly`});
+                        response.end("success");
+                    } else {
+                        response.statusCode = 200;
+                        response.end("fail");
+                    }
+                });
+            } else if (request.url === '/members') {
+                let body = [];
+                request.on('data', (chunk) => {
+                    body.push(chunk);
+                }).on('end', async () => {
+                    body = Buffer.concat(body).toString();
+                    const { id, password } = JSON.parse(body);
+                    if (await signupController.signup(id, password)) {
+                        response.statusCode = 200;
+                        response.end("success");
+                    } else {
+                        response.statusCode = 200;
+                        response.end("fail");
+                    }
+                });
+            }
+            break;
     }
 });
 
