9d2cda40531a8eb5078a2d2bdb14297e21546489
nailerHeum <kingman330@gmail.com>
2019-07-02 10:18:30 +0900

    Implement home directory route   home directory code 작성

--- server/app.js
+++ server/app.js
@@ -3,9 +3,25 @@ const http = require('http');
 const server = http.createServer();
 
 server.on('request', (request, response) => {
-
-})
+  const { method, url } = request;
+  console.log(`method : ${ method }, url: ${ url }`);
+  const resObject = {};
+  if (method === "GET" && url === "/") {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    resObject.action = '여기가 홈페이지여';
+    response.write(JSON.stringify(resObject));
+    response.end();
+    return;
+  }
+  response.statusCode = 404;
+  response.setHeader('Content-Type', 'application/json');
+  resObject.action = '여긴 암것도 업따. 돌아가렴';
+  response.write(JSON.stringify(resObject));
+  response.end();
+  return;
+});
 
 server.listen(3000, () => {
-  
-})
+  console.log('3000번 포트에서 서버 열림...');
+});
