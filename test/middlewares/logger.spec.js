9631717ca25e3ef9f9f98cf99781a0130665666c
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-02 17:04:00 +0900

    Test : logger 테스트 작성
    해당하는 request 메서드 별로 색상을 달리해 log 출력

--- test/middlewares/logger.spec.js
+++ test/middlewares/logger.spec.js
@@ -1,1 +1,29 @@
 const logger = require('../../middlewares/logger');
+const httpMocks = require('node-mocks-http');
+const sinon = require('sinon');
+
+describe('logger Test', () => {
+  let response;
+  let request;
+  let next;
+
+  beforeEach(() => {
+    response = httpMocks.createResponse();
+    request = httpMocks.createRequest();
+    next = () => {};
+  })
+
+  afterEach(() => {
+    request = null;
+    response = null;
+  })
+
+  it('메서드 별로 설정된 색깔 문자열 출력', () => {
+    request.method = 'GET';
+    request.url = 'path'
+    sinon.spy(console, 'log');
+    logger()(request, response, next);
+    sinon.assert.calledWith(console.log, `\x1b[33m[${request.method}]\x1b[0m ${request.url}`)
+  })
+
+})
