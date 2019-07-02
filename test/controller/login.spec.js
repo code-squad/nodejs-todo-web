a9ae124a2f294560cec9645a63570f5356cd5707
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-02 13:40:25 +0900

    Test : TestController getPage() 테스트 작성
    정상적으로 정적 파일이 내려지는 지 테스트

--- test/controller/login.spec.js
+++ test/controller/login.spec.js
@@ -1,2 +1,30 @@
+const should = require('should');
+const httpMocks = require('node-mocks-http');
 const loginController = require('../../controller/login');
 
+describe('loginController Test', () => {
+
+  beforeEach(() => {
+    response = httpMocks.createResponse();
+    request = httpMocks.createRequest();
+    next = () => {};
+  })
+
+  afterEach(() => {
+    request = null;
+    response = null;
+  })
+
+  describe('getPage()', () => {
+    it('getPage()를 요청하면 status 200과 content-type으로 text/html을 내려준다', async () => {
+      const next = () => {};
+      await loginController.getPage()(request, response, next);
+      const contentType = await response.getHeader('Content-Type');
+      const statusCode = await response.statusCode;
+
+      should(contentType).equal('text/html');
+      should(statusCode).equal(200);
+    })
+  })
+
+})
