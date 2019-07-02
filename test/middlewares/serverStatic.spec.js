856ff119d614434775cab6688567373d599257d3
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-02 17:23:27 +0900

    Test : serveStatic 모듈 테스트 코드 작성
    유효한 자원일 경우 200 응답, 해당 파일의 mimeType으로 content-type 설정
    유효한 자원이 아닐 경우 404 응답으로 Not Found File이라는 문자열 반환

--- test/middlewares/serverStatic.spec.js
+++ test/middlewares/serverStatic.spec.js
@@ -1,2 +1,41 @@
 const should = require('should');
+const httpMocks = require('node-mocks-http');
 const serveStatic = require('../../middlewares/serve-static');
+
+describe('serveStatic middleware Test', () => {
+  
+  let response;
+  let request;
+  let next;
+
+  beforeEach(() => {
+    response = httpMocks.createResponse();
+    request = httpMocks.createRequest();
+    next = () => {};
+  })
+
+  afterEach(() => {
+    request = null;
+    response = null;
+  })
+
+  it('유효한 자원일 경우 200 응답, 해당 파일의 mimeType으로 content-type 설정', async () => {
+    request.url = '/index.html';
+    await serveStatic()(request, response, next);
+
+    const contentType = await response.getHeader('Content-Type');
+    const statusCode = await response.statusCode;
+
+    should(contentType).equal('text/html');
+    should(statusCode).equal(200);
+  })
+
+  it('유효한 자원이 아닐 경우 404 응답으로 Not Found File이라는 문자열 반환', async () => {
+    request.url = '/unkonown.html';
+    await serveStatic()(request, response, next);
+
+    const statusCode = await response.statusCode;
+    should(contentType).equal('text/plain');
+    should(statusCode).equal(404);
+  })
+})
