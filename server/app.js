281bb2ff1268a4b0ded9b95c2d8e62eb15477e41
nailerHeum <kingman330@gmail.com>
2019-07-02 16:39:47 +0900

    Implement main page features
      main page에서 로그인 post 요청 또는 회원가입 선택 기능 구현

--- server/app.js
+++ server/app.js
@@ -1,23 +1,46 @@
 const http = require('http');
-
 const server = http.createServer();
+const fs = require('fs');
+const qs = require('querystring');
 
 server.on('request', (request, response) => {
   const { method, url } = request;
   console.log(`method : ${ method }, url: ${ url }`);
-  const resObject = {};
+  let data = '';
+  let body = [];
+
   if (method === "GET" && url === "/") {
     response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    resObject.action = '여기가 홈페이지여';
-    response.write(JSON.stringify(resObject));
+    //response.setHeader('Content-Type', 'application/json');
+    data = fs.readFileSync(__dirname + '/public/index.html')
+    response.write(data);
     response.end();
     return;
   }
+  
+  if (method === "GET" && url === "/signIn") {
+
+  }
+
+  if (method === "POST" && url === "/session") {
+
+  }
+  if (method === "POST" && url === "/user") {
+    
+    request.on('data', (chunk) => {
+      body.push(chunk);
+      console.log('Im here')
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      console.log(body);
+      let { username, password } = qs.parse(body);
+    });
+    
+  }
+
   response.statusCode = 404;
   response.setHeader('Content-Type', 'application/json');
-  resObject.action = '여긴 암것도 업따. 돌아가렴';
-  response.write(JSON.stringify(resObject));
+  //response.write(JSON.stringify(resObject));
   response.end();
   return;
 });
