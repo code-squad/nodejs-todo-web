3ce44ef83e11711538191b5d75720408037ca797
MCC <cocahack@naver.com>
2019-07-03 17:38:09 +0900

    refactor: console.log 제거

--- app/middlewares/cookieparser.js
+++ app/middlewares/cookieparser.js
@@ -1,7 +1,6 @@
 const { parseCookie } = require('../../util');
 
 module.exports = (req, res, next) => {
-  console.log(`쿠키 값: ${req.headers.cookie}`);
   const cookies = parseCookie(req.headers.cookie);
   req.sessionId = cookies ? cookies.token : undefined;
   next();
