831fb45b064b1265c0a1ad2bfb0563967d26e2e5
mukeunzi <eunji057@gmail.com>
2019-07-04 03:55:23 +0900

    feat : request url 의 response 를 생성하는 모듈
    - get, post, put, delete, use 함수 생성

--- js/server.js
+++ js/server.js
@@ -1,114 +1,115 @@
-const http = require('http');
 const path = require('path');
 const fs = require('./file.js');
 const member = require('./member.js');
 const todos = require('./todos.js');
+const urlList = require('../data/url.js');
 
-const port = 8000;
+const publicPath = path.join(__dirname, '..');
 
-const server = http.createServer(async (req, res) => {
-	const method = req.method;
-	const url = req.url;
-	const publicPath = path.join(__dirname, '..');
-	let ext = path.parse(req.url).ext;
+try {
+	module.exports.get = async (url, req, res) => {
+		const ext = path.parse(url).ext;
 
-	if (!ext) {
-		ext = '.html';
-	}
-
-	try {
-		if (url === '/') {
-			const { file, mimeType } = await fs.readFile(`${publicPath}/index.html`, ext);
-			res.writeHead(200, { 'Content-Type': mimeType });
-			res.end(file);
-		} else if (url === '/auth') {
-			if (method === 'GET') {
-				const { file, mimeType } = await fs.readFile(`${publicPath}/login.html`, ext);
-				res.writeHead(200, { 'Content-Type': mimeType });
-				res.end(file);
-			} else if (method === 'POST') {
-				req.on('data', loginData => {
-					const user_sid = member.login(loginData);
-					if (!user_sid) {
-						res.end('false');
-					} else {
-						res.writeHead(200, { 'Set-Cookie': [`sid=${user_sid}; Max-Age=${60 * 60 * 24}; HttpOnly;`] });
-						res.end('true');
-					}
-				});
-			}
-		} else if (url === '/users') {
-			if (method === 'GET') {
-				const { file, mimeType } = await fs.readFile(`${publicPath}/sign-up.html`, ext);
-				res.writeHead(200, { 'Content-Type': mimeType });
-				res.end(file);
-			} else if (method === 'POST') {
-				req.on('data', signUpData => {
-					const { user_sid, user_id } = member.signUp(signUpData);
-					todos.createUserArea(user_id);
-					if (!user_sid) {
-						res.end('false');
-					}
-					res.writeHead(302, { 'Set-Cookie': [`sid=${user_sid}; Max-Age=${60 * 60 * 24}; HttpOnly;`], Location: '/' });
-					res.end();
-				});
-			} else if (method === 'DELETE') {
-				member.logout(req.headers.cookie);
-				res.writeHead(302, { 'Set-Cookie': [`sid=; Max-Age=0; HttpOnly;`], Location: '/' });
-				res.end();
-			}
-		} else if (url === '/permission' && method === 'GET') {
-			if (!req.headers.cookie) {
-				res.end('false');
-			} else {
-				const userId = member.getUserId(req.headers.cookie);
-				if (!userId) {
-					res.end('false');
-				}
-				res.end(userId);
-			}
-		} else if (url === '/todo' && method === 'POST') {
-			req.on('data', addTodoData => {
-				const addedTodo = todos.addTodo(addTodoData);
-				res.end(JSON.stringify(addedTodo));
-			});
-		} else if (url.startsWith('/todos')) {
-			if (method === 'GET') {
-				const user_id = url.split('/')[2];
-				const todosList = todos.getTodosList(user_id);
-				res.end(JSON.stringify(todosList));
-			} else if (method === 'DELETE') {
-				const user_id = url.split('/')[2];
-				const todos_id = url.split('/')[3];
-				const deleteTodos = { user_id, todos_id };
-				todos.deleteTodos(deleteTodos);
-				res.end();
-			}
-		} else if (url.startsWith('/events')) {
-			if (method === 'PUT') {
-				const user_id = url.split('/')[2];
-				req.on('data', updateTodosData => {
-					todos.sortingTodosList(user_id, updateTodosData);
-					res.end();
-				});
-			}
-		} else if (url === '/error-500' && method === 'GET') {
-			const { file, mimeType } = await fs.readFile(`${publicPath}/error-500.html`, ext);
-			res.writeHead(200, { 'Content-Type': mimeType });
-			res.end(file);
-		} else {
+		if (ext) {
 			const { file, mimeType } = await fs.readFile(`${publicPath}${url}`, ext);
 			res.writeHead(200, { 'Content-Type': mimeType });
-			res.end(file);
+			return res.end(file);
 		}
-	} catch (error) {
-		console.log('error.....', error);
-		const { file, mimeType } = await fs.readFile(`${publicPath}/error-404.html`, ext);
-		res.writeHead(404, { 'Content-Type': mimeType });
-		res.end(file);
-	}
-});
+		if (url === '/error-500') {
+			// use(url, req, res);
+		}
+		if (readFileUrl(url)) {
+			const fileName = readFileUrl(url);
+			const { file, mimeType } = await fs.readFile(`${publicPath}${fileName}`, '.html');
+			res.writeHead(200, { 'Content-Type': mimeType });
+			return res.end(file);
+		}
+		if (url === '/permission') {
+			if (!req.headers.cookie) {
+				return res.end('false');
+			}
+			const userId = member.getUserId(req.headers.cookie);
+			if (!userId) {
+				return res.end('false');
+			}
+			return res.end(userId);
+		}
+		if (url.startsWith('/todos')) {
+			const user_id = url.split('/')[2];
+			const todosList = todos.getTodosList(user_id);
+			return res.end(JSON.stringify(todosList));
+		}
+	};
 
-server.listen(port, () => {
-	console.log(`start server ${port} port....!!`);
-});
+	module.exports.post = async (url, req, res) => {
+		if (url === '/auth') {
+			req.on('data', loginData => {
+				const user_sid = member.login(loginData);
+				if (!user_sid) {
+					return res.end('false');
+				}
+				res.writeHead(200, { 'Set-Cookie': [`sid=${user_sid}; Max-Age=${60 * 60 * 24}; HttpOnly;`] });
+				return res.end('true');
+			});
+		}
+		if (url === '/users') {
+			req.on('data', signUpData => {
+				const { user_sid, user_id } = member.signUp(signUpData);
+				todos.createUserArea(user_id);
+				if (!user_sid) {
+					return res.end('false');
+				}
+				res.writeHead(302, { 'Set-Cookie': [`sid=${user_sid}; Max-Age=${60 * 60 * 24}; HttpOnly;`], Location: '/' });
+				return res.end();
+			});
+		}
+		if (url === '/todo') {
+			req.on('data', addTodoData => {
+				const addedTodo = todos.addTodo(addTodoData);
+				return res.end(JSON.stringify(addedTodo));
+			});
+		}
+	};
+
+	module.exports.put = async (url, req, res) => {
+		if (url.startsWith('/events')) {
+			const user_id = url.split('/')[2];
+			req.on('data', updateTodosData => {
+				todos.sortingTodosList(user_id, updateTodosData);
+				return res.end();
+			});
+		}
+	};
+
+	module.exports.del = async (url, req, res) => {
+		if (url === '/users') {
+			member.logout(req.headers.cookie);
+			res.writeHead(302, { 'Set-Cookie': [`sid=; Max-Age=0; HttpOnly;`], Location: '/' });
+			return res.end();
+		}
+		if (url.startsWith('/todos')) {
+			const user_id = url.split('/')[2];
+			const todos_id = url.split('/')[3];
+			const deleteTodos = { user_id, todos_id };
+			todos.deleteTodos(deleteTodos);
+			return res.end();
+		}
+	};
+
+	module.exports.use = async (url, req, res) => {
+		const statusCode = url.slice(6);
+		const { file, mimeType } = await fs.readFile(`${publicPath}${url}.html`, '.html');
+		res.writeHead(statusCode, { 'Content-Type': mimeType });
+		return res.end(file);
+	};
+
+	const readFileUrl = url => {
+		if (Object.keys(urlList).includes(url)) {
+			return urlList[url];
+		}
+		return false;
+	};
+} catch (error) {
+	console.log('error .....', error);
+	use('/error-404', req, res);
+}
