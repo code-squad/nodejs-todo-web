af0127953e4d02df25255a493662114ce01f74a9
nailerHeum <kingman330@gmail.com>
2019-07-04 17:21:52 +0900

    [FEAT] user.js post, delete method
      post, delete 구현

--- server/test/user.spec.js
+++ server/test/user.spec.js
@@ -12,6 +12,33 @@ describe('POST /user', () => {
   it('이미 존재하는 유저', async () => {
     const query = { username : 'nailer', password : '12345', passwordConfirm : '12345' };
     const response = await user.exec["POST"](query);
-     response.should.deep.equals({ statusCode: 203, message: '이미 존재하는 유저입니다.'});
+    response.should.deep.equals({ statusCode: 203, message: '이미 존재하는 유저입니다.'});
   });
 });
+
+describe('GET /user', async () => {
+  it('유저 조회하기', async () => {
+    const username = 'nana';
+    const response = await user.exec["GET"](username);
+    response.should.deep.equals({ statusCode: 202, message: '유저가 존재합니다.', username: 'nana' });
+  });
+  it('없는 유저입니다.', async () => {
+    const username = 'stephens';
+    const response = await user.exec["GET"](username);
+    response.should.deep.equals({ statusCode: 203, message: '존재하지 않는 유저입니다.' });
+  });
+});
+
+describe('DELETE /user:id', async () => {
+  const mockSessions = [{ sid : '125236281432', name : 'nana' }, 
+                      { sid: '5123412352134', name : 'nailer'},
+                      { sid : '125236281432', name : 'kaka' },];
+  const newUser = { username : 'kaka', password : '12345', passwordConfirm: '12345' };
+  await user.exec["POST"](newUser);
+  it('유저 삭제하기', async () => {
+    const query = { sid : '125236281432', name : 'kaka' };
+    const response = await user.exec["DELETE"](query, mockSessions);
+    response.should.deep.equals({ statusCode: 202, message: '유저를 삭제했습니다.'});
+  });
+});
+
--- server/user.js
+++ server/user.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcrypt');
 const fs = require('fs');
 const saltRounds = 4;
+const rawData = fs.readFileSync(`./data/user.json`);
+const users = JSON.parse(rawData);
+
 
 class User {
   constructor() {
@@ -8,8 +11,6 @@ class User {
   }
 
   async post(query) {
-    const rawData = fs.readFileSync(`./data/user.json`)
-    const users = await JSON.parse(rawData);
     const { username, password, passwordConfirm } = query;
     if ( password !== passwordConfirm) {
       return { statusCode: 203, message: '비밀번호가 일치하지 않습니다.' };
@@ -24,14 +25,24 @@ class User {
     return { statusCode: 202, message: '회원 가입 완료!' };
   }
 
-  get() {
-  
+  async get(username) {
+    if (users.some(tmpuser => tmpuser.name === username)) {
+      return { statusCode: 202, message: '유저가 존재합니다.', username: username };
+    }
+    return { statusCode: 203, message: '존재하지 않는 유저입니다.' };
   }
-  delete() {
 
+  async delete(query, sessions) {
+    const { sid, name } = query;
+    const delIdx = sessions.findIndex(el => el.sid === sid && el.name === name);
+    // 임시 세션
+    if (delIdx === -1) {
+      return { statusCode: 203, message: '잘못된 접근입니다.' };
+    }
+    await users.splice(delIdx, 1);
+    fs.writeFileSync(`./data/user.json`, JSON.stringify(users));
+    return { statusCode: 202, message: '유저를 삭제했습니다.'};
   }
-  
-
 }
 
 module.exports = User;
