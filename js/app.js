6fb53cbc914b4aac230e43ee8613b51f364b7b80
mukeunzi <eunji057@gmail.com>
2019-07-04 17:48:16 +0900

    feat : app.js try-catch 문 추가

--- js/app.js
+++ js/app.js
@@ -3,24 +3,29 @@ const myApp = require('./server.js');
 
 const port = 8000;
 
-const server = http.createServer((req, res) => {
-	const method = req.method;
-	const url = req.url;
+try {
+	const server = http.createServer((req, res) => {
+		const method = req.method;
+		const url = req.url;
 
-	if (method === 'GET') {
-		myApp.get(url, req, res);
-	}
-	if (method === 'POST') {
-		myApp.post(url, req, res);
-	}
-	if (method === 'PUT') {
-		myApp.put(url, req, res);
-	}
-	if (method === 'DELETE') {
-		myApp.del(url, req, res);
-	}
-});
+		if (method === 'GET') {
+			myApp.get(url, req, res);
+		}
+		if (method === 'POST') {
+			myApp.post(url, req, res);
+		}
+		if (method === 'PUT') {
+			myApp.put(url, req, res);
+		}
+		if (method === 'DELETE') {
+			myApp.del(url, req, res);
+		}
+	});
 
-server.listen(port, () => {
-	console.log(`start server ${port} port....!!`);
-});
+	server.listen(port, () => {
+		console.log(`start server ${port} port....!!`);
+	});
+} catch (error) {
+	console.log('error .....', error);
+	myApp.use('/error-404', req, res);
+}
