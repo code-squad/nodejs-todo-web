6d6564be18e79cd5b5d1ff77c520f9c3ba5c3b2f
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-04 21:31:33 +0900

    [feat] 웹 페이지 디자인을 위한 html, css 수정

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -2,11 +2,11 @@ document.querySelector('#signUpButton').addEventListener('click', () => {
     const id = document.querySelector('#userID').value;
     const pw = document.querySelector('#userPW').value;
     const body = JSON.stringify({ id : id, pw : pw });
-    fetch('http://localhost:8888/signUpCheck', { method : 'post', body : body })
+    fetch('http://localhost:8888/signUpCheck', { method : 'post', redirect : 'follow', body : body })
     .then(response => {
         switch (response.status) {
-            case 200: alert(`존재 하는 회원 정보 입니다!`); break;
-            case 301: alert(`회원 가입 완료!`); window.location = '/'; break;
+            case 403: alert(`존재 하는 회원 정보 입니다!`); break;
+            case 302: alert(`회원 가입 완료!`); window.location.href = '/'; break;
             default: alert(`HTTP status : ${response.status}`); break;
         }
     });
