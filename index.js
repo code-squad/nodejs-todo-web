f50e3bd51fcd9488998f7a57cdd4bb2ff9f4cf89
SangYun Ha <hsycamp@gmail.com>
2019-07-05 12:13:17 +0900

    [H] step9 TODO App Webpage 미션 제출합니다. (#10)
    * feat : index.html 파일 생성
    
    todo 앱을 위한 기본 레이아웃 구성
    
    * feat : index.css 파일 생성
    
    * refactor : html 구조 변경
    
    todo-board에만 card를 추가할 수 있도록 변경
    
    * feat : todo card input 창 css 추가
    
    * feat : index.js 파일 생성
    
    click 이벤트 발생 시 card 입력 창을 보여주는 메소드 작성
    
    * feat : todo card를 생성하는 메소드 작성
    
    * feat : todo 리스트에 card를 추가하는 메소드 작성
    
    * feat : todo 리스트에 card 추가 후 card 입력창을 닫는 메소드 작성
    
    * feat : drag 이벤트 메소드 작성
    
    각 리스트 내부에서 card들이 이동할 수 있도록하는 메소드 구현
    
    * refactor : css 파일 수정
    
    중복 코드 제거
    
    * refactor : html 구조 변경
    
    빈 리스트에 card를 drop할 수 있도록 하기위해 default card를 추가
    
    * feat : 빈 리스트에 card를 drop할 수 있도록 구현
    
    * refactor : card 입력창에서 입력값 없이 엔터키만 칠 경우 입력창을 닫도록 구현
    
    입력창 나타내기, 숨기기 기능 메소드 이름 변경
    
    * feat : card 삭제 기능 구현
    
    delete 버튼 클릭시 해당 card를 삭제하는 기능 구현
    
    * feat : card 내용 수정 기능 구현
    
    editCard 메소드 추가
    
    * refactor : 유저가 화면상의 text를 선택할 수 없도록 수정
    
    * style : css 파일 수정
    
    main-board 넓이 수정
    
    * refactor : drop 이벤트의 타겟을 선택하는 로직 변경
    
    * refactor : 카드 내용 수정시 입력창에 이전 내용이 표시되어 출력되도록 수정
    
    * form : 코드 정렬

---
+++ index.js
@@ -0,0 +1,150 @@
+let draggingTarget = null;
+
+const createNewCard = function(input) {
+  const newCard = document.createElement("div");
+  const cardText = document.createElement("div");
+  const deleteButton = document.createElement("button");
+  const editText = document.createElement("input");
+  const editButton = document.createElement("button");
+
+  newCard.setAttribute("class", "todo-card");
+  newCard.setAttribute("draggable", true);
+  cardText.setAttribute("class", "card-text");
+  cardText.innerText = input;
+  deleteButton.innerText = "Delete";
+  deleteButton.setAttribute("class", "delete-card-btn");
+  editText.setAttribute("value", input);
+  editText.setAttribute("id", "edit-text");
+  editText.setAttribute("type", "text");
+  editButton.innerText = "Edit";
+  editButton.setAttribute("class", "edit-card-btn");
+
+  newCard.appendChild(cardText);
+  newCard.appendChild(deleteButton);
+  newCard.appendChild(editText);
+  newCard.appendChild(editButton);
+
+  addDragEvent(newCard);
+
+  deleteButton.addEventListener("click", function(event) {
+    deleteCard(event);
+  });
+  editButton.addEventListener("click", function(event) {
+    showEditInputBox(event);
+  });
+  createEnterKeyEvent(editText, editCard);
+  return newCard;
+};
+
+const addNewCard = function() {
+  const inputText = document.querySelector("#todo-input-text");
+  if (!inputText.value) return hideInputTextBox();
+  const todoList = document.querySelector(".todo-list");
+  const todoBoard = todoList.querySelector(".board");
+  const newCard = createNewCard(inputText.value);
+  todoBoard.appendChild(newCard);
+  inputText.value = "";
+  hideInputTextBox();
+};
+
+const deleteCard = function(event) {
+  const targetCard = event.target.parentNode;
+  const targetList = targetCard.parentNode;
+  targetList.removeChild(targetCard);
+};
+
+const editCard = function(event) {
+  const targetCard = event.target.parentNode;
+  const cardText = targetCard.querySelector(".card-text");
+  const editTextBox = event.target;
+  if (!editTextBox.value) {
+    editTextBox.value = cardText.innerText;
+    return hideEditInputBox(event);
+  }
+  cardText.innerText = editTextBox.value;
+  hideEditInputBox(event);
+};
+
+const showEditInputBox = function(event) {
+  const editInputBox = event.target.previousSibling;
+  editInputBox.style.display = "block";
+  editInputBox.focus();
+};
+
+const hideEditInputBox = function(event) {
+  event.target.style.display = "none";
+};
+
+const createEvent = function() {
+  const todoList = document.querySelector(".todo-list");
+  const doingList = document.querySelector(".doing-list");
+  const doneList = document.querySelector(".done-list");
+  addDragEvent(todoList);
+  addDragEvent(doingList);
+  addDragEvent(doneList);
+
+  const addButton = document.querySelector(".add-card-btn");
+  addButton.addEventListener("click", function(event) {
+    showInputTextBox();
+  });
+
+  const inputText = document.querySelector("#todo-input-text");
+  createEnterKeyEvent(inputText, addNewCard);
+};
+
+const createEnterKeyEvent = function(inputText, eventHandler) {
+  inputText.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13) {
+      eventHandler(event);
+    }
+  });
+};
+
+const showInputTextBox = function() {
+  const target = document.querySelector("#todo-input-text");
+  target.style.display = "block";
+  target.focus();
+};
+
+const hideInputTextBox = function() {
+  const target = document.querySelector("#todo-input-text");
+  target.style.display = "none";
+};
+
+const addDragEvent = function(card) {
+  card.addEventListener("dragstart", function(event) {
+    draggingTarget = event.target;
+    event.dataTransfer.setData("text/html", draggingTarget);
+  });
+
+  card.addEventListener("dragover", function(event) {
+    event.preventDefault();
+  });
+
+  card.addEventListener("dragenter", function(event) {
+    if (event.target.className === "board") {
+      event.target.style["border-bottom"] = "solid 60px rgb(161, 193, 253)";
+    }
+    if (event.target.className === "todo-card") {
+      event.target.style["border-top"] = "solid 15px darkblue";
+    }
+  });
+
+  card.addEventListener("dragleave", function(event) {
+    event.target.style["border-bottom"] = "";
+    event.target.style["border-top"] = "";
+  });
+
+  card.addEventListener("drop", function(event) {
+    event.target.style["border-top"] = "";
+    event.target.style["border-bottom"] = "";
+    if (event.target.className === "board") {
+      event.target.appendChild(draggingTarget);
+    }
+    if (event.target.className === "todo-card") {
+      event.target.parentNode.insertBefore(draggingTarget, event.target);
+    }
+  });
+};
+
+createEvent();
