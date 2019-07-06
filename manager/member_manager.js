00539ee9d628c42e7357b99286223a60747d8b72
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-06 17:25:54 +0900

    [refactor] 함수형 방식에서 클래스로 변경

--- manager/member_manager.js
+++ manager/member_manager.js
@@ -1,23 +1,28 @@
-const fs = require('fs');
+class MemberManager {
+    constructor(fs) {
+        this.fs = fs;
+    }
 
-module.exports.readMemberInfo = () => {
-    return new Promise((resolve) => {
-        console.time(`[ Member Mananger ] Read member information `);
-        fs.readFile('./db/member_Information.csv', 'utf-8', (error, data) => {
-            if (error) throw error;
-            resolve(`{${data.substr(0, data.length - 1)}}`);
+    readMemberInfo() {
+        return new Promise((resolve) => {
+            console.time(`[ Member Mananger ] Read member information `);
+            this.fs.readFile('./db/member_Information.csv', 'utf-8', (error, data) => {
+                if (error) throw error;
+                resolve(`{${data.substr(0, data.length - 1)}}`);
+            });
+            console.timeEnd(`[ Member Mananger ] Read member information `);
         });
-        console.timeEnd(`[ Member Mananger ] Read member information `);
-    });
+    }
+
+    writeMemberInfo(input) {
+        console.time(`[ Member Mananger ] Save member information `);
+        const writeData = `"${input.id}":"${input.pw}",`;
+        const option = { encoding: 'utf-8', flag: 'a' };
+        this.fs.appendFile('./db/member_Information.csv', writeData, option, (error) => {
+            if (error) throw error;
+        });
+        console.timeEnd(`[ Member Mananger ] Save member information `);
+    }
 }
 
-module.exports.writeMemberInfo = async (input) => {
-    console.time(`[ Member Mananger ] Save member information `);
-    const writeData = `"${input.id}":"${input.pw}",`;
-    const option = { encoding: 'utf-8', flag: 'a' };
-    fs.appendFile('./db/member_Information.csv', writeData, option, (error) => {
-        if (error) throw error;
-        console.log("Member is appended to file successfully.");
-    });
-    console.timeEnd(`[ Member Mananger ] Save member information `);
-}
+module.exports = MemberManager;
--- manager/todoList_manager.js
+++ manager/todoList_manager.js
@@ -1,33 +1,38 @@
-const fs = require('fs');
+class TodoListManager {
+    constructor(fs) {
+        this.fs = fs;
+    }
 
-module.exports.initTodoList = async (id) => {
-    console.time(`[ TodoList Mananger ] Init todoList `);
-    const writeData = `"${id}":{"todo":[],"doing":[],"done":[]},`;
-    const option = { encoding: 'utf-8', flag: 'a' };
-    fs.appendFile('./db/todoList_Information.csv', writeData, option, (error) => {
-        if (error) throw error;
-    });
-    console.timeEnd(`[ TodoList Mananger ] Init todoList `);
-}
-
-module.exports.writeTodoList = async (todoList) => {
-    console.time(`[ TodoList Mananger ] write todoList `);
-    const option = { encoding: 'utf-8', flag: 'w' };
-    todoList = todoList.substr(1, todoList.length - 2) + ",";
-    fs.writeFile('./db/todoList_Information.csv', todoList, option, (error) => {
-        if (error) throw error;
-    });
-    console.timeEnd(`[ TodoList Mananger ] write todoList `);
-}
-
-module.exports.readTodoList = () => {
-    return new Promise((resolve) => {
-        console.time(`[ TodoList Mananger ] Read TodoList `);
-        fs.readFile('./db/todoList_Information.csv', 'utf-8', (error, data) => {
-            if (error) throw error;
-            resolve(`{${data.substr(0, data.length - 1)}}`);
+    readTodoList() {
+        return new Promise((resolve) => {
+            console.time(`[ TodoList Mananger ] Read TodoList `);
+            this.fs.readFile('./db/todoList_Information.csv', 'utf-8', (error, data) => {
+                if (error) throw error;
+                resolve(`{${data.substr(0, data.length - 1)}}`);
+            });
+            console.timeEnd(`[ TodoList Mananger ] Read TodoList `);
         });
-        console.timeEnd(`[ TodoList Mananger ] Read TodoList `);
-    });
+    }
+
+    initTodoList(id) {
+        console.time(`[ TodoList Mananger ] Init todoList `);
+        const writeData = `"${id}":{"todo":[],"doing":[],"done":[]},`;
+        const option = { encoding: 'utf-8', flag: 'a' };
+        this.fs.appendFile('./db/todoList_Information.csv', writeData, option, (error) => {
+            if (error) throw error;
+        });
+        console.timeEnd(`[ TodoList Mananger ] Init todoList `);
+    }
+
+    writeTodoList(todoList) {
+        console.time(`[ TodoList Mananger ] write todoList `);
+        const option = { encoding: 'utf-8', flag: 'w' };
+        todoList = todoList.substr(1, todoList.length - 2) + ",";
+        this.fs.writeFile('./db/todoList_Information.csv', todoList, option, (error) => {
+            if (error) throw error;
+        });
+        console.timeEnd(`[ TodoList Mananger ] write todoList `);
+    }
 }
 
+module.exports = TodoListManager;
