ee3f09b92ffff29a5d690c5a0f772d6055abc0ac
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-06 18:06:57 +0900

    [refactor] manager 객체를 만들어서 사용하도록 변경

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const todoListManager   = require('./manager/todoList_manager');
-const memberManager     = require('./manager/member_manager');
+const SessionManager    = require('./manager/session_manager');
+const TodoListManager   = require('./manager/todoList_manager');
+const MemberManager     = require('./manager/member_manager');
 const utilCookies       = require('./util/cookie_parser');
 const staticFile        = require('./static_file');
 const httpStatus        = require('./http_status');
 const http              = require('http');
+const fs                = require('fs');
 
-const sessionTable = new Map();
+const sessionManager    = new SessionManager(utilCookies);
+const todoListManager   = new TodoListManager(fs);
+const memberManager     = new MemberManager(fs);
 
 const receiveData = (request) => {
     return new Promise((resolve) => {
@@ -14,45 +18,26 @@ const receiveData = (request) => {
     });
 }
 
-const isExistSessionID = async (cookie) => {
-    return cookie !== undefined && sessionTable.has(utilCookies.parse(cookie).SID);
-}
-
 const isExistPassword = async (input) => {
     const member = JSON.parse(await memberManager.readMemberInfo());
     return (member[input.id] === input.pw) ? true : false;
-}
-
-const isNotExistMember = async (input) => {
-    const member = JSON.parse(await memberManager.readMemberInfo());
-    return (member[input.id] === undefined) ? true : false;
-}
-
-// TODO : crypto 모듈 사용해서 세션 ID를 만들도록 변경할 것..
-const createSessionID = async (inputID) => {
-    let sessionID = 0;
-    while (true) {
-        const min = 100000000000000000, max = 999999999999999999;
-        sessionID = String(Math.floor(Math.random() * (max - min + 1)) + min);
-        if (!sessionTable.has(sessionID)) {
-            sessionTable.set(sessionID, inputID);
-            console.log(`[ Server ] session Table size : ${sessionTable.size}`);
-            break;
-        }
-    }
-    return sessionID;
 }
 
 const signIn = async (request, response) => {
     console.time(`[ Server ] Sign In process `);
     const input = await receiveData(request);
     if (await isExistPassword(input)) {
-        const cookieInfo = [`SID=${await createSessionID(input.id)}; Max-Age=${60 * 60 * 24 * 30}`];
+        const cookies = [`SID=${sessionManager.createSession(input.id)}; Max-Age=${sessionManager.getMaxAge()}; HttpOnly`];
         response.statusCode = httpStatus.FOUND;
-        response.setHeader('Set-Cookie', cookieInfo);
+        response.setHeader('Set-Cookie', cookies);
     } else response.statusCode = httpStatus.FORBIDDEN;
     response.end();
     console.timeEnd(`[ Server ] Sign In process `);
+}
+
+const isNotExistMember = async (input) => {
+    const member = JSON.parse(await memberManager.readMemberInfo());
+    return (member[input.id] === undefined) ? true : false;
 }
 
 const signUp = async (request, response) => {
@@ -69,11 +54,12 @@ const signUp = async (request, response) => {
 
 const signOut = async (request, response) => {
     console.time(`[ Server ] Sign Out process `);
-    if (request.headers.cookie !== undefined) {
-        const sessionID = utilCookies.parse(request.headers.cookie).SID;
-        if (sessionTable.has(sessionID)) sessionTable.delete(sessionID);
+    if(sessionManager.isValid(request.headers.cookie)) {
+        const sessionID = sessionManager.getSession(request.headers.cookie);
+        sessionManager.deleteSession(sessionID);
     }
-    redirect(response, '/');
+    response.statusCode = httpStatus.FOUND;
+    response.end();
     console.timeEnd(`[ Server ] Sign Out process `);
 }
 
@@ -82,80 +68,77 @@ const error = async (response) => {
     response.end();
 }
 
-const redirect = async (response, URL) => {
+const redirect = async (response, url) => {
     response.statusCode = httpStatus.FOUND;
-    response.setHeader('Location', `${URL}`);
+    response.setHeader('Location', `${url}`);
+    response.end();
+}
+/////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////
+const addItem = async (request, response) => {
+    const data      = await receiveData(request);
+    const sessionID = sessionManager.getSession(request.headers.cookie);
+    const todoList  = JSON.parse(await todoListManager.readTodoList());
+    const userID    = JSON.parse(sessionManager.getValue(sessionID)).id;
+    todoList[userID][data.type].splice(data.index, 0, data.value);
+    todoListManager.writeTodoList(JSON.stringify(todoList));
     response.end();
 }
 
-////////////////////////////////////////////////////////////////////////////
-// command ..
-const addItem = async (request, response) => {
-    const data = await receiveData(request);
-    const sessionID = utilCookies.parse(request.headers.cookie).SID;
-    if (sessionTable.has(sessionID)) {
-        const jsonData = JSON.parse(await todoListManager.readTodoList());
-        const todoList = jsonData[sessionTable.get(sessionID)];
-        todoList[data.type].splice(data.index, 0, data.value);
-        todoListManager.writeTodoList(JSON.stringify(jsonData));
-        response.end();
-    } else throw Error(`[Server - Error] Session ID is not saved after Login. ( Add Item )`);
-}
-
 const deleteItem = async (request, response) => {
-    const data = await receiveData(request);
-    const sessionID = utilCookies.parse(request.headers.cookie).SID;
-    if (sessionTable.has(sessionID)) {
-        const jsonData = JSON.parse(await todoListManager.readTodoList());
-        const todoList = jsonData[sessionTable.get(sessionID)];
-        todoList[data.type].splice(data.index, 1);
-        todoListManager.writeTodoList(JSON.stringify(jsonData));
-        response.end();
-    } else throw Error(`[Server - Error] Session ID is not saved after Login. ( Delete Item )`);
+    const data      = await receiveData(request);
+    const sessionID = sessionManager.getSession(request.headers.cookie);
+    const todoList  = JSON.parse(await todoListManager.readTodoList());
+    const userID    = JSON.parse(sessionManager.getValue(sessionID)).id;
+    todoList[userID][data.type].splice(data.index, 1);
+    todoListManager.writeTodoList(JSON.stringify(todoList));
+    response.end();
 }
 
 const updateItem = async (request, response) => {
-    const data = await receiveData(request);
-    const sessionID = utilCookies.parse(request.headers.cookie).SID;
-    if (sessionTable.has(sessionID)) {
-        const jsonData = JSON.parse(await todoListManager.readTodoList());
-        const todoList = jsonData[sessionTable.get(sessionID)];
-        const [ addValue ] = todoList[data.deleteType].splice(data.deleteIndex, 1);
-        if (data.deleteType === data.addType && data.deleteIndex < data.addIndex) data.addIndex--;
-        todoList[data.addType].splice(data.addIndex, 0, addValue);
-        todoListManager.writeTodoList(JSON.stringify(jsonData));
-        response.end();
-    } else throw Error(`[Server - Error] Session ID is not saved after Login. ( Delete Item )`);
+    const data      = await receiveData(request);
+    const sessionID = sessionManager.getSession(request.headers.cookie);
+    const todoList  = JSON.parse(await todoListManager.readTodoList());
+    const userID    = JSON.parse(sessionManager.getValue(sessionID)).id;
+    const [addValue] = todoList[userID][data.deleteType].splice(data.deleteIndex, 1);
+    if (data.deleteType === data.addType && data.deleteIndex < data.addIndex) data.addIndex--;
+    todoList[userID][data.addType].splice(data.addIndex, 0, addValue);
+    todoListManager.writeTodoList(JSON.stringify(todoList));
+    response.end();
 }
 
 const showItem = async (request, response) => {
-    const sessionID = utilCookies.parse(request.headers.cookie).SID;
-    if (sessionTable.has(sessionID)) {
-        const jsonData = JSON.parse(await todoListManager.readTodoList());
-        const todoList = jsonData[sessionTable.get(sessionID)];
-        response.write(JSON.stringify(todoList));
-        response.end();
-    } else throw Error(`[Server - Error] Session ID is not saved after Login. ( Show Item )`);
+    const sessionID = sessionManager.getSession(request.headers.cookie);
+    const todoList  = JSON.parse(await todoListManager.readTodoList());
+    const userID    = JSON.parse(sessionManager.getValue(sessionID)).id;
+    response.write(JSON.stringify(todoList[userID]));
+    response.end();
 }
-////////////////////////////////////////////////////////////////////////////
 
 const post = async (request, response) => {
-    switch (request.url) {
-        case '/add'         : addItem(request, response);    break;
-        case '/delete'      : deleteItem(request, response); break;
-        case '/update'      : updateItem(request, response); break;
-        case '/signInCheck' : signIn(request, response);     break;
-        case '/signUpCheck' : signUp(request, response);     break;
-        case '/signOut'     : signOut(request, response);    break;
-        default             : error(response);               break;
+    if (request.url === '/signOut') signOut(request, response);
+    else if (sessionManager.isValid(request.headers.cookie)) {
+        switch (request.url) {
+            case '/add'     : addItem(request, response);       break;
+            case '/delete'  : deleteItem(request, response);    break;
+            case '/update'  : updateItem(request, response);    break;
+            default         : error(response);                  break;
+        }
+    } else {
+        switch (request.url) {
+            case '/signInCheck' : signIn(request, response);        break;
+            case '/signUpCheck' : signUp(request, response);        break;
+            default             : error(response);                  break;
+        }
     }
 }
 
 const get = async (request, response) => {
-    if (await isExistSessionID(request.headers.cookie)) {
-        switch (request.url) { 
-            case '/'        : 
-            case '/signIn?' : 
+    if (sessionManager.isValid(request.headers.cookie)) {
+        switch (request.url) {
+            case '/'        :
+            case '/signIn?' :
             case '/signUp?' : redirect(response, '/todoList');          break;
             case '/show'    : showItem(request, response);              break;
             default         : staticFile.load(request.url, response);   break;
