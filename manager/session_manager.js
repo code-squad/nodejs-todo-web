51e9ea23fdbca51350f3c61772f8ecca1e1b252f
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-06 17:28:58 +0900

    [feat] 세션을 관리하는 클래스 추가

---
+++ manager/session_manager.js
@@ -0,0 +1,57 @@
+class SessionMananger {
+    constructor(utilCookies) {
+        this.utilCookies = utilCookies;
+        this.sessionTable = new Map();
+        this.second = 60;
+        this.minute = 60;
+        this.hour   = 24;
+        this.day    = 30;
+    }
+
+    createSession(inputID) {
+        let sessionID = 0;
+        while (true) {
+            const min = 1e15, max = Number.MAX_SAFE_INTEGER;
+            sessionID = String(Math.floor(Math.random() * (max - min + 1)) + min);
+            if (!this.sessionTable.has(sessionID)) {
+                this.sessionTable.set(sessionID, JSON.stringify({ id : inputID, date : new Date()}));
+                console.log(`Session Table Size : ${this.sessionTable.size}`);
+                break;
+            }
+        }
+        return sessionID;
+    }
+
+    getSession(cookie) {
+        return this.utilCookies.parse(cookie).SID
+    }
+
+    deleteSession(sessionID) {
+        this.sessionTable.delete(sessionID);
+    }
+
+    getValue(sessionID) {
+        return this.sessionTable.get(sessionID);
+    }
+
+    getMaxAge() {
+        return this.second * this.minute * this.hour * this.day;
+    }
+
+    isValid(cookie) {
+        if (cookie === undefined) return false;
+        const sessionID = this.getSession(cookie);
+        if (this.sessionTable.has(sessionID)) {
+            const initMS    = new Date(JSON.parse(this.getValue(sessionID)).date).getTime();
+            const currentMS = new Date().getTime();
+            const limitMS   = 18e5;
+            if (currentMS - initMS > limitMS) {
+                this.deleteSession(sessionID);
+                return false;
+            }
+        } else return false;
+        return true;
+    }
+}
+
+module.exports = SessionMananger;
