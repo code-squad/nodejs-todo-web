0a6f25342858e8734057fe8c3bc87e89eb702a95
Ryujeuk <jeuk817@gmail.com>
2019-07-07 20:55:13 +0900

    feat: Middleware에 add, run 메소드 추가
    1. add하면 this.middlewares에 함수가 추가되는 메소드 구현
    2. run하면 req, res를 constructor에 할당하고, _run(0)을 실행하는 메소드
    구현
    3. _run(i)하면 next를 _run(i+1)로 선언하고, middlewares[i]를
    selectedFunc으로 선언하여 실행하도록 구현

--- src/Middleware.js
+++ src/Middleware.js
@@ -1,7 +1,30 @@
 class Middleware {
     constructor() {
         this.middlewares = [];
+        this.req;
+        this.res;
     }
+
+    add(fn) {
+        this.middlewares.push(fn);
+    }
+
+    _run(i) {
+        if (i < 0 || i >= this.middlewares.length) return;
+
+        const selectedFunc = this.middlewares[i];
+        const next = () => this._run(i + 1);
+
+        selectedFunc(this.req, this.res, next);
+    }
+
+    run(req, res) {
+        this.req = req;
+        this.res = res;
+
+        this._run(0);
+    }
+
 }
 
 module.exports = Middleware;
