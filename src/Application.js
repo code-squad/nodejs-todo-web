111ca6a5098230639ae6374287739953ab74c8ba
Ryujeuk <jeuk817@gmail.com>
2019-07-07 16:22:16 +0900

    feat: style.css, dynamic.js 적용
    1. setHeader의 Content-Type에 적용하기위해 mimeType을 객체로 생성
    2. path.join으로 public 디렉토리까지의 경로 조합
    3. 앞서 조합한 경로와 req.url(ex: /css/style.css)를 조합해 fs.readFile로
    파일 데이터를 가져왔다.
    4. 앞서 얻은 path.parse(req.url).ext (ex: '.css')를 키값으로 mimeType을
    가져와 res.setHeader에 Content-Type으로 설정한다.
    5. 그리고 res.end(data)로 출력하여 결과물을 확인하였다.

--- src/Application.js
+++ src/Application.js
@@ -5,14 +5,39 @@ const fs = require('fs');
 class Application {
     constructor() {
         this.server = http.createServer((req, res) => {
-            const filePath = path.join(__dirname, '../public/web.html');
-            fs.readFile(filePath, (err, data) => {
-                if (err) throw err;
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data);
-            })
-            // res.end('hello world in Application')
+            const mimeType = {
+                '.ico': 'image/x-icon',
+                '.html': 'text/html',
+                '.js': 'text/javascript',
+                '.css': 'text/css',
+                '.png': 'image/png',
+                '.jpg': 'image/jpeg',
+                '.eot': 'appliaction/vnd.ms-fontobject',
+                '.ttf': 'aplication/font-sfnt'
+            };
+            const publicPath = path.join(__dirname, '../public')
+            const ext = path.parse(req.url).ext;
+
+            if (Object.keys(mimeType).includes(ext)) {
+                fs.readFile(`${publicPath}${req.url}`, (err, data) => {
+                    if (err) {
+                        res.statusCode = 404;
+                        res.end('Not found');
+                    } else {
+                        res.statusCode = 200
+                        res.setHeader('Content-Type', mimeType[ext]);
+                        res.end(data)
+                    }
+                });
+            } else {
+                const filePath = path.join(__dirname, '../public/web.html')
+                fs.readFile(filePath, (err, data) => {
+                    if (err) throw err;
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(data);
+                })
+            }
         })
     }
 
