f272e6803e9eda3dd967df8fb02d4a37b16e9361
hsycamp <hsycamp@gmail.com>
2019-07-07 15:54:14 +0900

    feat : 정적 파일 요청에 응답하는 기능 구현

--- src/application.js
+++ src/application.js
@@ -1,11 +1,19 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const Application = () => {
   const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World\n");
+    res.setHeader("Content-Type", "text/html");
+
+    const filePath = path.join(__dirname, "../public/index.html");
+    fs.readFile(filePath, (err, data) => {
+      if (err) throw err;
+      res.end(data);
+    });
   });
+
   const listen = (port = 3000, hostname = "127.0.0.1", fn) => {
     server.listen(port, hostname, fn);
   };
