6c338d5f5ea32e3629189b6186553f9ecbf2f92c
mukeunzi <eunji057@gmail.com>
2019-07-07 17:05:51 +0900

    feat : error page url 추가

--- url.js
+++ url.js
@@ -1,5 +1,7 @@
 module.exports = {
 	'/': '/index.html',
 	'/auth': '/login.html',
-	'/users': '/sign-up.html'
+	'/users': '/sign-up.html',
+	'/error-404': '/error-404.html',
+	'/error-500': '/error-500.html'
 };
