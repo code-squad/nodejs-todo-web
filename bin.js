badfd405bfe80dbfcc859afef528e40d8850c57b
Ryujeuk <jeuk817@gmail.com>
2019-07-07 22:34:35 +0900

    feat, refactor: middleware를 활용하여 serve-static를 분리하여 적용
    1. src/Application에 this.server에서 정적파일을 가죠우는 로직을 middleware.run으로 수정.
    2. middleware.add를 수행하는 use메소드 추가
    3. middlewares/serve-static.js 생성
    4. 정적파일 불러오는 함수 serveStatic구현(없으면 다음 미들웨어를
    수행하도록 next()추가) 및 익스포트
    5. bin.js에 serve-static임포트후 app.use()로 실행
    6. bin.js에 web.html을 출력하는 함수 web 선언 후 app.use()로 실행

--- bin.js
+++ bin.js
@@ -1,8 +1,24 @@
 const app = require('./src/Application');
-// const App = require('./src/Application');
-// const app = new App();
+const serveStatic = require('./middlewares/serve-static');
+const path = require('path');
+const fs = require('fs');
 const port = 3000;
 const hostName = '127.0.0.1';
+
+const web = (req, res, next) => {
+    serveStatic(req, res);
+    const filePath = path.join(__dirname, './public/web.html');
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) throw err;
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+    })
+}
+
+app.use(serveStatic());
+app.use(web);
 
 app.listen(port, hostName, () => {
     console.log(`${port}번 포트에서 서버 대기중입니다.`)
