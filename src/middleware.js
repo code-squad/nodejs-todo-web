4df8c4b113a65b48d6d5133315dd4d4068a1f646
hsycamp <hsycamp@gmail.com>
2019-07-07 21:23:01 +0900

    feat : 미들웨어 실행 함수 추가

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,29 @@
 const middleware = () => {
   const middleware = [];
 
+  const middlewareRun = (i, err) => {
+    if (i < 0 || i >= middlewares.length) return;
+
+    const nextMiddleWare = middlewares[i];
+    const next = err => run(i + 1, err);
+
+    nextMiddleWare(req, res, next);
+  };
+
   const add = func => {
     middleware.push(func);
   };
 
+  const run = (req, res) => {
+    const req = req;
+    const res = res;
+    middlewareRun(0, null);
+  };
+
   return {
     middleware,
-    add
+    add,
+    run
   };
 };
 
