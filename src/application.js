5bc97f77b43b2b53017d57d23947e7f36db1ab7c
hsycamp <hsycamp@gmail.com>
2019-07-08 21:15:40 +0900

    feat : 미들웨어를 이용해 use 함수 구현

--- src/application.js
+++ src/application.js
@@ -1,27 +1,22 @@
 const http = require("http");
-const serveStatic = require("./serve-static");
-const path = require("path");
-const fs = require("fs");
+const Middleware = require("./middleware");
 
 const Application = () => {
+  const middleware = Middleware();
   const server = http.createServer((req, res) => {
-    serveStatic(req, res);
-    const publicPath = path.join(__dirname, "../public");
-    fs.readFile(`${publicPath}/index.html`, (err, data) => {
-      if (err) throw err;
-
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end(data);
-    });
+    middleware.run(req, res);
   });
+  const use = func => {
+    middleware.add(func);
+  };
 
-  const listen = (port = 3000, hostname = "127.0.0.1", fn) => {
-    server.listen(port, hostname, fn);
+  const listen = (port = 3000, hostname = "127.0.0.1", func) => {
+    server.listen(port, hostname, func);
   };
 
   return {
     server,
+    use,
     listen
   };
 };
