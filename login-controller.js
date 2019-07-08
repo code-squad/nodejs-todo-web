3b1bfc8a82bee8aeabcced973d1a6cc174c7fa6e
Jimin Yeon <gmini.y@gmail.com>
2019-07-08 21:31:41 +0900

    [feat] 잘못된 ID 입력시 로그인 메세지 출력 안되는 에러 픽스

--- login-controller.js
+++ login-controller.js
@@ -4,6 +4,7 @@ const loginController = {
     async login(id, password) {
         const members = JSON.parse(await model.readStaticFile('./members.json'));
         const member = members.find((element) => element.id === id);
+        if(member === undefined) return false;
         if(member.password === password) return true;
         return false;
     }
