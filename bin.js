a4a94951003d3d49a2241cbadf2f190cabae45cc
Ryujeuk <jeuk817@gmail.com>
2019-07-10 00:24:40 +0900

    feat: bodyParser 구현
    파싱하는 로직이 없으면 없어서 request의 body데이터는 스트림으로
    오기때문에 읽어오지 못한다.
    그래서 bodyParser를 만들어 스트림을 문자로 만든후 다시 객체로 변환하도록
    구현했다.
    그리고 제대로 데이터가 넘어오는지 확인했다.

--- bin.js
+++ bin.js
@@ -2,11 +2,13 @@ const app = require('./src/Application');
 const serveStatic = require('./middlewares/serve-static');
 const index = require('./routes/index');
 const logger = require('./middlewares/logger');
+const bodyParser = require('./middlewares/body-parser');
 const port = 3000;
 const hostName = '127.0.0.1';
 
 
 app.use(logger());
+app.use(bodyParser());
 app.use(serveStatic());
 app.use('/', index.listPosts());
 app.use('/signup', index.signUp());
