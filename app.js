1f67dc87c9bc8fc44d2c1053d6c171cf0cdec644
hsycamp <hsycamp@gmail.com>
2019-07-09 12:31:16 +0900

    feat : 요청 정보를 로깅하는 logger 미들웨어 구현

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require("fs");
 
 const serveStatic = require("./middlewares/serve-static");
+const logger = require("./middlewares/logger");
 const App = require("./src/Application");
 const app = App();
 
@@ -27,6 +28,7 @@ const error = (err, req, res, next) => {
   res.end();
 };
 
+app.use(logger());
 app.use(serveStatic());
 app.use(index);
 app.use(error404);
