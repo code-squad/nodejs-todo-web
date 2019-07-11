0ba599d38db08417164f2c14c2b6556252f698f5
nailerHeum <kingman330@gmail.com>
2019-07-11 15:23:03 +0900

    [FEAT] init routes files
      indexController & userController

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const App = require('./application');
+const indexController = require('./routes/indexController');
+const userController = require('./routes/userController');
 const { serveStaticFile } = require('./middlewares/public-files');
 const app = new App();
 
 app.use(serveStaticFile);
-
-
-
+app.use('/', indexController);
+app.use('/user', userController);
 module.exports = app;
