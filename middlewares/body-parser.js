ba56f1298f6e859075b3f8b200ddec70579bc140
hsycamp <hsycamp@gmail.com>
2019-07-13 16:53:33 +0900

    feat : body parser 구현

---
+++ middlewares/body-parser.js
@@ -0,0 +1,24 @@
+const bodyParser = () => (req, res, next) => {
+  let body = [];
+
+  req.on("data", chunk => {
+    body.push(chunk);
+    console.log("data", chunk);
+  });
+
+  req.on("end", () => {
+    body = Buffer.concat(body).toString();
+
+    body = body.split("&").reduce((body, pair) => {
+      if (!pair) return body;
+      const frg = pair.split("=");
+      body[frg[0]] = frg[1];
+      return body;
+    }, {});
+
+    req.body = body;
+    next();
+  });
+};
+
+module.exports = bodyParser;
