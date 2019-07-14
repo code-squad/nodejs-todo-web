d6af714db00e3891088ad49e476a88277d972460
hsycamp <hsycamp@gmail.com>
2019-07-15 00:11:13 +0900

    fix : session 미들웨어 수정

--- middlewares/session.js
+++ middlewares/session.js
@@ -1,15 +1,20 @@
-const sessionDb = require("../db/session-db");
+const db = require("../api/db-handler");
 
 const session = () => (req, res, next) => {
   const cookie = req.headers.cookie;
+  
   if (cookie === undefined) {
     req.session = "false";
     next();
     return;
   }
   const sid = cookie.split("sid=")[1];
+  const sessionId = db
+    .get("session")
+    .find({ sid: sid })
+    .value();
 
-  if (sessionDb[sid]) {
+  if (sessionId) {
     req.session = {
       sid: sid
     };
