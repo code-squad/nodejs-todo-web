0089e46bce8c068a54bd64cac7d836c9406e55ff
LeeJuyeon <ruby413@hanmail.net>
2019-07-15 16:43:42 +0900

    feat : add error template

--- view/template.js
+++ view/template.js
@@ -19,20 +19,17 @@ class Show{
             ${list}
         </div>
         <script>
-        const pw = document.getElementById("pw")
-        const pw_check = document.getElementById("pw_check")
-        const signUp = document.getElementById("signUp")
-        signUp.addEventListener("click", () => {pw.value !== pw_check.value ? alert("비밀번호 정보와 비밀번호 확인이 다릅니다.") : signUp.type = 'summit'});
+        
         </script>
-        <script type="text/javascript" src="./public/login.js"></script>
         </body>
         </html>
         `;
     }
 
-    loginTemplate(){
+    loginTemplate(status){
         let template =  
-        `<div class = "wrap">
+        `${status}
+        <div class = "wrap">
         <form action="/login_process" method="POST" class="form">
         <p><input type="text" name="email" placeholder="email" class="idpw"></p>
         <p><input type="password" name="pwd" placeholder="password" class="idpw"></p>
@@ -45,9 +42,10 @@ class Show{
         return template;
     }
 
-    signupTemplate(){
+    signupTemplate(status){
         let template = 
-        `<div class = "wrap">
+        `${status}
+        <div class = "wrap">
         <form action="/signup_process" method="post" class="form">
         <p><input type="text" name="nickName" placeholder="nickName"  class="idpw"></p>
         <p><input type="text" name="email" placeholder="email"  class="idpw"></p>
@@ -55,11 +53,25 @@ class Show{
         <p><input type="password" name="passwordCheck" placeholder="password Check" class="idpw" id="pw_check"></p>
         <p>
           <a href="/login"  class="loginSignup signup signup_login">login</a>
-          <input type="button" value="signUp" class="loginSignup signup_signup" id= "signUp">
+          <input type="submit" value="signUp" class="loginSignup signup_signup" id= "signUp">
         </p>
         </form>
         </div>` 
         return template;
+    }
+
+    loginError(){
+        let template = `
+        <p class = "error">아이디와 비밀번호 정보가 다릅니다.</p>
+        `
+        return template
+    }
+
+    signUpError(){
+        let template = `
+        <p class = "error">이미 등록되어 있는 가입자 정보입니다.</p>
+        `
+        return template
     }
 }
 
