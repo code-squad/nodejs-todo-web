134f874782962048bd24267524da96b0b89435e4
hsycamp <hsycamp@gmail.com>
2019-07-17 03:43:28 +0900

    feat : 로그인 페이지 접근시, req.session값을 확인해 todo 페이지로 redirect해주도록 구현

--- routers/sign-in.js
+++ routers/sign-in.js
@@ -3,11 +3,17 @@ const fs = require("fs");
 const fileHandler = require("../api/file-handler");
 
 const getSignInPage = () => async (req, res, next) => {
-  const publicPath = path.join(__dirname, "../public");
-  const data = await fileHandler.readFile(`${publicPath}/sign-in.html`);
-
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(data);
+  if (req.session) {
+    res.writeHead(302, {
+      Location: "/todo"
+    });
+    res.end();
+  } else {
+    const publicPath = path.join(__dirname, "../public");
+    const data = await fileHandler.readFile(`${publicPath}/sign-in.html`);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  }
 };
 
 module.exports = {
