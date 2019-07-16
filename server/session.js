dee6cba6deae5c059a741d21bd38aa27c9a338cd
nailerHeum <kingman330@gmail.com>
2019-07-16 14:44:42 +0900

    [FEAT] post method of /session
      session list에 sid와 username을 추가시킨다.

--- server/session.js
+++ server/session.js
@@ -1,12 +1,16 @@
+const uuidv4 = require('uuid/v4');
+
 class Session {
   constructor() {
-    this.exec = { "GET" : this.get, "POST": this.post, "UPDATE": this.update, "DELETE": this.del }
+    this.exec = { "GET" : this.get, "POST": this.post, "UPDATE": this.update, "DELETE": this.del };
+    this.list = [];
   }
   get() {
     
   }
-  post() {
-
+  post(username) {
+    this.list.push({ username : username, id: uuidv4() });
+    return;
   }
   update() {
 
--- server/test/session.spec.js
+++ server/test/session.spec.js
@@ -1,12 +1,23 @@
 const should = require('chai').should();
 const Session = require('../session');
 
-describe('POST session', () => {
-  it('username를 받아서 세션을 생성한다.', () => {
+describe('POST /session', () => {
+  it('username의 정보를 session list에 추가한다.', async () => {
     const session = new Session();
-    const username = 'nailer';
-    session.post(username);
-    const postedSes = session.list.find(ses => ses.username === username);
-    postedSes.username.should.equals(username);
+    await session.post('nailer');
+    let postedSes = session.list.find(ses => ses.username === 'nailer');
+    postedSes.username.should.equals('nailer');
+    await session.post('seunghee');
+    postedSes = session.list.find(ses => ses.username === 'seunghee');
+    postedSes.username.should.equals('seunghee');
+  });
+  it('user의 sid를 생성하여 session에 추가한다.', async () => {
+    const session = new Session();
+    await session.post('nailer');
+    const nailerSes = session.list.find(ses => ses.username === 'nailer');
+    nailerSes.id.should.be.a('string');
+    await session.post('seunghee');
+    const heeSes = session.list.find(ses => ses.username === 'seunghee');
+    heeSes.id.should.not.equals(nailerSes.id);
   });
 });
